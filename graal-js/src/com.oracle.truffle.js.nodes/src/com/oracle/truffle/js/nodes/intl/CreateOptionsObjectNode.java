e45a5996e1b813056ed630417b0f8b5d379381e2
Jakub Podlesak <jakub.podlesak@oracle.com>
2018-02-01 12:55:32 +0100

    forgot to update a couple of copyright year headers

--- graal-js/src/com.oracle.truffle.js.nodes/src/com/oracle/truffle/js/nodes/intl/CreateOptionsObjectNode.java
+++ graal-js/src/com.oracle.truffle.js.nodes/src/com/oracle/truffle/js/nodes/intl/CreateOptionsObjectNode.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2018, Oracle and/or its affiliates. All rights reserved.
+ * ORACLE PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
 package com.oracle.truffle.js.nodes.intl;
 
 import com.oracle.truffle.api.CompilerDirectives;
