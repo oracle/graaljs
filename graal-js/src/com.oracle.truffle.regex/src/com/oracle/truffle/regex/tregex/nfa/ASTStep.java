261b1aca87ca21404fe1ea22b24cf84c49eda648
Jirka Marsik <jiri.marsik@oracle.com>
2018-02-06 14:53:49 +0100

    Cap the number of ASTSuccessors in a single ASTStep

--- graal-js/src/com.oracle.truffle.regex/src/com/oracle/truffle/regex/tregex/nfa/ASTStep.java
+++ graal-js/src/com.oracle.truffle.regex/src/com/oracle/truffle/regex/tregex/nfa/ASTStep.java
@@ -5,6 +5,8 @@
 package com.oracle.truffle.regex.tregex.nfa;
 
 import com.oracle.truffle.api.CompilerDirectives;
+import com.oracle.truffle.regex.UnsupportedRegexException;
+import com.oracle.truffle.regex.tregex.TRegexOptions;
 import com.oracle.truffle.regex.tregex.parser.ast.RegexASTNode;
 import com.oracle.truffle.regex.tregex.util.DebugUtil;
 
@@ -29,6 +31,18 @@ public final class ASTStep {
 
     public void addSuccessor(ASTSuccessor successor) {
         successors.add(successor);
+        // When compiling a regular expression, almost all ASTSuccessors will yield at least 1 NFA
+        // transition (the only case when an ASTSuccessor yields no NFA transitions is when the NFA
+        // transition would collide with a position assertion such as $ or ^, as in /(?=a$)ab/, see
+        // NFAGenerator#createNFATransitions). Furthermore, there exist regular expressions such as
+        // (a?|b?|c?|d?|e?|f?|g?)(a?|b?|c?|d?|e?|f?|g?)... The number of ASTSuccessors in a single
+        // ASTStep rises exponentially with the number of repetitions of this pattern (there is a
+        // different ASTSuccessor for every possible path to a next matching character). If we want
+        // to avoid running out of memory in such situations, we have to bailout during the
+        // collection of ASTSuccessors in ASTStep, before they are transformed into NFA transitions.
+        if (successors.size() > TRegexOptions.TRegexMaxNumberOfASTSuccessorsInOneASTStep) {
+            throw new UnsupportedRegexException("TRegex Bailout: ASTSuccessor explosion");
+        }
     }
 
     @CompilerDirectives.TruffleBoundary
