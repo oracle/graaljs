84585a5471a87ea95ade0fc00d9feb598f14ca01
Daniele Bonetta <daniele.bonetta@oracle.com>
2018-02-20 11:00:50 -0500

    JSLazyString implements TruffleObject.

---
+++ graal-js/src/com.oracle.truffle.js.runtime/src/com/oracle/truffle/js/runtime/truffleinterop/JSLazyStringForeignAccessFactory.java
@@ -0,0 +1,93 @@
+/*
+ * Copyright (c) 2018, 2018, Oracle and/or its affiliates. All rights reserved.
+ * ORACLE PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package com.oracle.truffle.js.runtime.truffleinterop;
+
+import com.oracle.truffle.api.interop.CanResolve;
+import com.oracle.truffle.api.interop.MessageResolution;
+import com.oracle.truffle.api.interop.Resolve;
+import com.oracle.truffle.api.interop.TruffleObject;
+import com.oracle.truffle.api.nodes.Node;
+import com.oracle.truffle.js.runtime.objects.JSLazyString;
+
+@MessageResolution(receiverType = JSLazyString.class)
+public class JSLazyStringForeignAccessFactory {
+
+    @Resolve(message = "READ")
+    abstract static class ReadNode extends Node {
+        public Object access(JSLazyString target, int key) {
+            return target.charAt(key);
+        }
+    }
+
+    @Resolve(message = "GET_SIZE")
+    abstract static class GetSizeNode extends Node {
+        public int access(JSLazyString target) {
+            return target.length();
+        }
+    }
+
+    @Resolve(message = "UNBOX")
+    abstract static class UnboxNode extends Node {
+        public Object access(JSLazyString target) {
+            return target.toString();
+        }
+    }
+
+    @Resolve(message = "IS_BOXED")
+    abstract static class IsBoxedNode extends Node {
+        @SuppressWarnings("unused")
+        public Object access(JSLazyString target) {
+            return true;
+        }
+    }
+
+    @Resolve(message = "IS_EXECUTABLE")
+    abstract static class IsExecutableNode extends Node {
+        @SuppressWarnings("unused")
+        public Object access(JSLazyString target) {
+            return false;
+        }
+    }
+
+    @Resolve(message = "IS_NULL")
+    abstract static class IsNullNode extends Node {
+        @SuppressWarnings("unused")
+        public Object access(JSLazyString target) {
+            return false;
+        }
+    }
+
+    @Resolve(message = "IS_INSTANTIABLE")
+    abstract static class IsInstantiableNode extends Node {
+        @SuppressWarnings("unused")
+        public Object access(JSLazyString target) {
+            return false;
+        }
+    }
+
+    @Resolve(message = "HAS_SIZE")
+    abstract static class HasSizeNode extends Node {
+        @SuppressWarnings("unused")
+        public Object access(JSLazyString target) {
+            return true;
+        }
+    }
+
+    @Resolve(message = "HAS_KEYS")
+    abstract static class HasKeysNode extends Node {
+        @SuppressWarnings("unused")
+        public Object access(JSLazyString target) {
+            return false;
+        }
+    }
+
+    @CanResolve
+    public abstract static class CanResolveNode extends Node {
+
+        protected boolean test(TruffleObject receiver) {
+            return receiver instanceof JSLazyString;
+        }
+    }
+}
