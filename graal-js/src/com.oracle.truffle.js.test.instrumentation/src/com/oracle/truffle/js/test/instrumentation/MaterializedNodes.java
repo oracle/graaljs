661cc3b71c833dea182cc0be0d39ac13994d06ac
Daniele Bonetta <daniele.bonetta@oracle.com>
2018-03-22 10:25:06 -0400

    Added test

--- graal-js/src/com.oracle.truffle.js.test.instrumentation/src/com/oracle/truffle/js/test/instrumentation/MaterializedNodes.java
+++ graal-js/src/com.oracle.truffle.js.test.instrumentation/src/com/oracle/truffle/js/test/instrumentation/MaterializedNodes.java
@@ -16,18 +16,22 @@ import com.oracle.truffle.api.instrumentation.StandardTags.StatementTag;
 import com.oracle.truffle.api.source.Source;
 import com.oracle.truffle.api.instrumentation.Tag;
 import com.oracle.truffle.js.nodes.JavaScriptNode;
+import com.oracle.truffle.js.nodes.access.GlobalConstantNode;
 import com.oracle.truffle.js.nodes.access.JSConstantNode;
+import com.oracle.truffle.js.nodes.access.JSTargetableNode;
 import com.oracle.truffle.js.nodes.function.AbstractFunctionArgumentsNode;
 import com.oracle.truffle.js.nodes.function.JSFunctionArgumentsNode;
 import com.oracle.truffle.js.nodes.function.JSFunctionCallNode;
+import com.oracle.truffle.js.nodes.function.JSFunctionCallNode.InvokeNode;
 import com.oracle.truffle.js.nodes.instrumentation.JSTags.FunctionCallExpressionTag;
 import com.oracle.truffle.js.runtime.AbstractJavaScriptLanguage;
+import com.oracle.truffle.js.runtime.objects.JSObject;
+import com.oracle.truffle.js.runtime.objects.Undefined;
 
 public class MaterializedNodes {
 
     @Test
     public void functionNode() {
-
         AbstractFunctionArgumentsNode args = JSFunctionArgumentsNode.create(new JavaScriptNode[]{});
         JSFunctionCallNode c = JSFunctionCallNode.create(JSConstantNode.createUndefined(), null, args, false, false);
         c.setSourceSection(Source.newBuilder("").name("").mimeType(AbstractJavaScriptLanguage.APPLICATION_MIME_TYPE).build().createUnavailableSection());
@@ -35,9 +39,27 @@ public class MaterializedNodes {
         s.add(FunctionCallExpressionTag.class);
         c.addStatementTag();
         JSFunctionCallNode m = (JSFunctionCallNode) c.materializeInstrumentableNodes(s);
-
         assertTrue(m.hasTag(StatementTag.class));
         assertTrue(!((InstrumentableNode) m.getTarget()).hasTag(StatementTag.class));
+    }
+
+    @Test
+    public void materializeMulti() {
+        JSTargetableNode undef = GlobalConstantNode.createGlobalConstant(JSObject.getJSContext(Undefined.instance), "test", Undefined.instance);
+        AbstractFunctionArgumentsNode args = JSFunctionArgumentsNode.create(new JavaScriptNode[]{});
+        JSFunctionCallNode c = JSFunctionCallNode.createInvoke(undef, args, false, false);
+        c.setSourceSection(Source.newBuilder("").name("").mimeType(AbstractJavaScriptLanguage.APPLICATION_MIME_TYPE).build().createUnavailableSection());
+        undef.setSourceSection(Source.newBuilder("").name("").mimeType(AbstractJavaScriptLanguage.APPLICATION_MIME_TYPE).build().createUnavailableSection());
+        Set<Class<? extends Tag>> s = new HashSet<>();
+        s.add(FunctionCallExpressionTag.class);
+        c.addStatementTag();
+        JSFunctionCallNode m = (JSFunctionCallNode) c.materializeInstrumentableNodes(s);
+        m = (JSFunctionCallNode) m.materializeInstrumentableNodes(s);
+        InvokeNode i = (InvokeNode) m;
+        assertTrue(i.getFunctionTargetNode().getSourceSection() != null);
+        assertTrue(m.hasTag(StatementTag.class));
+        assertTrue(!((InstrumentableNode) m.getTarget()).hasTag(StatementTag.class));
+        assertTrue(m.hasSourceSection());
     }
 
 }
