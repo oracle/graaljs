33cadd78c4424662a7ee8920a784c1475c7ad50f
Jan Stola <jan.stola@oracle.com>
2018-04-25 21:01:46 +0200

    Adding a missing specialization for Object.prototype.valueOf.call(largeInteger)

--- graal-js/src/com.oracle.truffle.js.builtins/src/com/oracle/truffle/js/builtins/ObjectPrototypeBuiltins.java
+++ graal-js/src/com.oracle.truffle.js.builtins/src/com/oracle/truffle/js/builtins/ObjectPrototypeBuiltins.java
@@ -207,6 +207,11 @@ public final class ObjectPrototypeBuiltins extends JSBuiltinsContainer.SwitchEnu
             return toObject(thisObj);
         }
 
+        @Specialization
+        protected DynamicObject valueOf(LargeInteger thisObj) {
+            return toObject(thisObj);
+        }
+
         @Specialization(guards = "!isTruffleObject(thisObj)")
         protected DynamicObject valueOf(Object thisObj) {
             return toObject(thisObj);
