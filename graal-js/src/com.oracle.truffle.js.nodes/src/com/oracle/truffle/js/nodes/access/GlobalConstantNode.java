63075376ba8c15637463a92b37f8cf6c28bfad8e
Jan Stola <jan.stola@oracle.com>
2018-08-16 10:00:48 +0200

    Correction of __DIR__ Nashorn extension

--- graal-js/src/com.oracle.truffle.js.nodes/src/com/oracle/truffle/js/nodes/access/GlobalConstantNode.java
+++ graal-js/src/com.oracle.truffle.js.nodes/src/com/oracle/truffle/js/nodes/access/GlobalConstantNode.java
@@ -203,10 +203,14 @@ public class GlobalConstantNode extends JSTargetableNode implements ReadNode {
             if (source.isInternal() || source.isInteractive()) {
                 return "";
             }
-            String path = source.getName();
+            String path = source.getPath();
+            path = (path == null) ? source.getName() : path;
             if (File.separatorChar == '\\' && path.startsWith("/")) {
                 // on Windows, remove first "/" from /c:/test/dir/ style paths
                 path = path.substring(1);
+            }
+            if (path.startsWith("file:")) {
+                path = path.substring("file:".length());
             }
             Path filePath = Paths.get(path).toAbsolutePath();
             String dirPath = filePath.getParent().toString();
