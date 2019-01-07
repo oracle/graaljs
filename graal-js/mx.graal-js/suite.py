720677575ce1591cbde38cc743b13ab4f8b6e97a
Andreas Woess <andreas.woess@oracle.com>
2019-01-07 15:54:35 +0100

    Set versions for next dev cycle.

--- graal-js/mx.graal-js/suite.py
+++ graal-js/mx.graal-js/suite.py
@@ -3,7 +3,7 @@ suite = {
 
   "name" : "graal-js",
 
-  "version" : "1.0.0-rc11",
+  "version" : "1.0.0-rc12",
   "release" : False,
   "groupId" : "org.graalvm.js",
   "url" : "http://www.graalvm.org/",
