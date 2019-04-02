affca578d08876ab161c15fe509711c5a26d78a9
Jan Stola <jan.stola@oracle.com>
2019-04-02 19:56:31 +0200

    EnumerateNode should return enumerateIterator (not JSArray).

--- graal-js/src/com.oracle.truffle.js/src/com/oracle/truffle/js/nodes/access/EnumerateNode.java
+++ graal-js/src/com.oracle.truffle.js/src/com/oracle/truffle/js/nodes/access/EnumerateNode.java
@@ -68,7 +68,6 @@ import com.oracle.truffle.js.runtime.JSArguments;
 import com.oracle.truffle.js.runtime.JSContext;
 import com.oracle.truffle.js.runtime.JSRuntime;
 import com.oracle.truffle.js.runtime.builtins.JSAdapter;
-import com.oracle.truffle.js.runtime.builtins.JSArray;
 import com.oracle.truffle.js.runtime.builtins.JSFunction;
 import com.oracle.truffle.js.runtime.objects.JSObject;
 import com.oracle.truffle.js.runtime.util.EnumerateIterator;
@@ -209,8 +208,6 @@ public abstract class EnumerateNode extends JavaScriptNode {
                 assert InteropLibrary.getFactory().getUncached().hasArrayElements(keysObj);
                 long longSize = keysInterop.getArraySize(keysObj);
                 return enumerateForeignNonArray(context, iteratedObject, keysObj, longSize, values, objInterop, keysInterop);
-            } else {
-                return JSArray.createEmptyZeroLength(context);
             }
         } catch (UnsupportedMessageException ex) {
             // swallow and default
