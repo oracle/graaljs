30af31eb951f3ace4f9e86bc7b22c3910df104af
Danilo Ansaloni <danilo.ansaloni@oracle.com>
2019-04-18 16:19:32 +0200

    [GR-15306] Release GraalVM 1.0.0-rc16.

--- graal-js/mx.graal-js/suite.py
+++ graal-js/mx.graal-js/suite.py
@@ -4,7 +4,7 @@ suite = {
   "name" : "graal-js",
 
   "version" : "1.0.0-rc16",
-  "release" : False,
+  "release" : True,
   "groupId" : "org.graalvm.js",
   "url" : "http://www.graalvm.org/",
   "developer" : {
