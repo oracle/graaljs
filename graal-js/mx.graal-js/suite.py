3fc479c29c7c3af6690dd06afc064ddd531156c7
Danilo Ansaloni <danilo.ansaloni@oracle.com>
2019-04-18 12:11:47 +0200

    [GR-15306] Set versions for next dev cycle.

--- graal-js/mx.graal-js/suite.py
+++ graal-js/mx.graal-js/suite.py
@@ -3,7 +3,7 @@ suite = {
 
   "name" : "graal-js",
 
-  "version" : "1.0.0-rc16",
+  "version" : "1.0.0-rc17",
   "release" : False,
   "groupId" : "org.graalvm.js",
   "url" : "http://www.graalvm.org/",
