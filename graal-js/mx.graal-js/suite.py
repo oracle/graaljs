9e769455f6413f78ba7ce65d4fbbac2d28ffd4ec
Josef Haider <josef.haider@oracle.com>
2019-04-24 22:24:18 +0200

    bump tregex import

--- graal-js/mx.graal-js/suite.py
+++ graal-js/mx.graal-js/suite.py
@@ -24,7 +24,7 @@ suite = {
         {
            "name" : "regex",
            "subdir" : True,
-           "version" : "4d0b3e6019ec9e9852d62fdf25620dcc25db0f47",
+           "version" : "7ae362669cab97dff3f7d74e796fac829e5b5658",
            "urls" : [
                 {"url" : "https://github.com/graalvm/graal.git", "kind" : "git"},
                 {"url" : "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind" : "binary"},
