971247addbda3854cacb80fd5df5b072989cc6dd
Jan Stola <jan.stola@oracle.com>
2019-08-16 15:09:50 +0200

    Improving the handling of arguments in the direct eval (spread operator should work there).

--- graal-js/src/com.oracle.truffle.js/src/com/oracle/truffle/js/nodes/function/EvalNode.java
+++ graal-js/src/com.oracle.truffle.js/src/com/oracle/truffle/js/nodes/function/EvalNode.java
@@ -56,6 +56,7 @@ import com.oracle.truffle.api.object.DynamicObject;
 import com.oracle.truffle.api.source.Source;
 import com.oracle.truffle.api.source.SourceSection;
 import com.oracle.truffle.js.lang.JavaScriptLanguage;
+import com.oracle.truffle.js.nodes.JavaScriptBaseNode;
 import com.oracle.truffle.js.nodes.JavaScriptNode;
 import com.oracle.truffle.js.nodes.instrumentation.JSTags.EvalCallTag;
 import com.oracle.truffle.js.runtime.BigInt;
@@ -70,21 +71,19 @@ import com.oracle.truffle.js.runtime.objects.Undefined;
 
 public abstract class EvalNode extends JavaScriptNode {
     private final JSContext context;
-    private final Object currEnv;
     @Child @Executed protected JavaScriptNode functionNode;
-    @Child @Executed protected JavaScriptNode sourceNode;
-    @Child private JavaScriptNode thisObject;
-    @Child private AbstractFunctionArgumentsNode otherArguments;
+    @Child protected AbstractFunctionArgumentsNode arguments;
+    @Child protected DirectEvalNode directEvalNode;
 
-    protected EvalNode(JSContext context, Object currEnv, JavaScriptNode function, JavaScriptNode source, JavaScriptNode thisObject, AbstractFunctionArgumentsNode otherArguments) {
-        assert currEnv != null;
+    protected EvalNode(JSContext context, Object currEnv, JavaScriptNode function, JavaScriptNode[] args, JavaScriptNode thisObject) {
+        this(context, function, JSFunctionArgumentsNode.create(context, args), DirectEvalNode.create(context, currEnv, thisObject));
+    }
 
+    protected EvalNode(JSContext context, JavaScriptNode functionNode, AbstractFunctionArgumentsNode arguments, DirectEvalNode directEvalNode) {
         this.context = context;
-        this.currEnv = currEnv;
-        this.functionNode = function;
-        this.sourceNode = source;
-        this.thisObject = thisObject;
-        this.otherArguments = otherArguments;
+        this.functionNode = functionNode;
+        this.arguments = arguments;
+        this.directEvalNode = directEvalNode;
     }
 
     @Override
@@ -97,41 +96,29 @@ public abstract class EvalNode extends JavaScriptNode {
     }
 
     @Specialization(guards = {"!isEvalOverridden(evalFunction)"})
-    protected Object directEvalCharSequence(VirtualFrame frame, @SuppressWarnings("unused") Object evalFunction, CharSequence sourceCode) {
-        evalOtherArgs(frame);
-        return directEvalImpl(frame, sourceCode);
+    protected Object evalNotOverridden(VirtualFrame frame, @SuppressWarnings("unused") Object evalFunction) {
+        int argCount = arguments.getCount(frame);
+        Object[] args = new Object[argCount];
+        args = arguments.executeFillObjectArray(frame, args, 0);
+        Object source = args.length == 0 ? Undefined.instance : args[0];
+        return directEvalNode.executeWithSource(frame, source);
     }
 
-    private Object directEvalImpl(VirtualFrame frame, CharSequence sourceCode) {
-        final Source source = sourceFromString(sourceCode);
-        return context.getEvaluator().evaluate(context.getRealm(), this, source, currEnv, frame.materialize(), thisObject.execute(frame));
+    @Specialization(guards = {"isEvalOverridden(evalFunction)"})
+    protected Object evalOverridden(VirtualFrame frame, Object evalFunction,
+                    @Cached("createCall()") JSFunctionCallNode redirectCall) {
+        int argCount = arguments.getCount(frame);
+        Object[] args = JSArguments.createInitial(Undefined.instance, evalFunction, argCount);
+        args = arguments.executeFillObjectArray(frame, args, JSArguments.RUNTIME_ARGUMENT_COUNT);
+        return redirectCall.executeCall(args);
     }
 
-    @Specialization(guards = {"!isEvalOverridden(evalFunction)", "isForeignObject(sourceCode)"}, limit = "3")
-    protected Object directEvalForeignObject(VirtualFrame frame, @SuppressWarnings("unused") Object evalFunction, TruffleObject sourceCode,
-                    @CachedLibrary("sourceCode") InteropLibrary interop) {
-        evalOtherArgs(frame);
-        if (interop.isString(sourceCode)) {
-            try {
-                return directEvalImpl(frame, interop.asString(sourceCode));
-            } catch (UnsupportedMessageException ex) {
-                throw Errors.createTypeErrorInteropException(sourceCode, ex, "asString", this);
-            }
-        } else {
-            return sourceCode;
-        }
+    protected final boolean isEvalOverridden(Object function) {
+        return function != context.getRealm().getEvalFunctionObject();
     }
 
-    @TruffleBoundary
-    private Source sourceFromString(CharSequence sourceCode) {
-        String evalSourceName = null;
-        if (context.isOptionV8CompatibilityMode()) {
-            evalSourceName = formatEvalOrigin(this);
-        }
-        if (evalSourceName == null) {
-            evalSourceName = Evaluator.EVAL_SOURCE_NAME;
-        }
-        return Source.newBuilder(JavaScriptLanguage.ID, sourceCode.toString(), evalSourceName).build();
+    public static EvalNode create(JSContext context, Object env, JavaScriptNode functionNode, JavaScriptNode[] args, JavaScriptNode thisObject) {
+        return EvalNodeGen.create(context, env, functionNode, args, thisObject);
     }
 
     @TruffleBoundary
@@ -161,94 +148,109 @@ public abstract class EvalNode extends JavaScriptNode {
         return Truffle.getRuntime().iterateFrames(frameInstance -> formatEvalOrigin(frameInstance.getCallNode()));
     }
 
-    @Specialization(guards = {"!isEvalOverridden(evalFunction)"})
-    protected int directEvalInt(VirtualFrame frame, @SuppressWarnings("unused") Object evalFunction, int arg0) {
-        evalOtherArgs(frame);
-        return arg0;
-    }
-
-    @Specialization(guards = {"!isEvalOverridden(evalFunction)"})
-    protected LargeInteger directEvalLargeInteger(VirtualFrame frame, @SuppressWarnings("unused") Object evalFunction, LargeInteger arg0) {
-        evalOtherArgs(frame);
-        return arg0;
-    }
-
-    @Specialization(guards = {"!isEvalOverridden(evalFunction)"})
-    protected long directEvalLong(VirtualFrame frame, @SuppressWarnings("unused") Object evalFunction, long arg0) {
-        evalOtherArgs(frame);
-        return arg0;
-    }
-
-    @Specialization(guards = {"!isEvalOverridden(evalFunction)"})
-    protected double directEvalDouble(VirtualFrame frame, @SuppressWarnings("unused") Object evalFunction, double arg0) {
-        evalOtherArgs(frame);
-        return arg0;
-    }
-
-    @Specialization(guards = {"!isEvalOverridden(evalFunction)"})
-    protected boolean directEvalBoolean(VirtualFrame frame, @SuppressWarnings("unused") Object evalFunction, boolean arg0) {
-        evalOtherArgs(frame);
-        return arg0;
-    }
-
-    @Specialization(guards = {"!isEvalOverridden(evalFunction)"})
-    protected Symbol directEvalSymbol(VirtualFrame frame, @SuppressWarnings("unused") Object evalFunction, Symbol arg0) {
-        evalOtherArgs(frame);
-        return arg0;
-    }
-
-    @Specialization(guards = {"!isEvalOverridden(evalFunction)"})
-    protected BigInt directEvalBigInt(VirtualFrame frame, @SuppressWarnings("unused") Object evalFunction, BigInt arg0) {
-        evalOtherArgs(frame);
-        return arg0;
-    }
-
-    @Specialization(guards = {"!isEvalOverridden(evalFunction)", "isJSType(arg0)"})
-    protected DynamicObject directEvalJSType(VirtualFrame frame, @SuppressWarnings("unused") Object evalFunction, DynamicObject arg0) {
-        evalOtherArgs(frame);
-        return arg0;
-    }
-
-    @Specialization(guards = {"isEvalOverridden(evalFunction)"})
-    protected Object directEvalOverridden(VirtualFrame frame, Object evalFunction, Object arg0,
-                    @Cached("createCall()") JSFunctionCallNode redirectCall) {
-        Object thisObj = Undefined.instance;
-        Object[] evaluatedArgs;
-        if (otherArguments == null) {
-            evaluatedArgs = new Object[]{arg0};
-        } else {
-            evaluatedArgs = new Object[1 + otherArguments.getCount(frame)];
-            evaluatedArgs[0] = arg0;
-            evaluatedArgs = otherArguments.executeFillObjectArray(frame, evaluatedArgs, 1);
-        }
-        return redirectCall.executeCall(JSArguments.create(thisObj, evalFunction, evaluatedArgs));
-    }
-
-    protected final boolean isEvalOverridden(Object function) {
-        return function != context.getRealm().getEvalFunctionObject();
-    }
-
-    /**
-     * Execute surplus arguments for the sake of side-effects.
-     */
-    private void evalOtherArgs(VirtualFrame frame) {
-        if (otherArguments != null) {
-            Object[] evaluatedArgs = new Object[otherArguments.getCount(frame)];
-            otherArguments.executeFillObjectArray(frame, evaluatedArgs, 0);
-        }
-    }
-
-    /**
-     * In case of multiple arguments, only the first one is used. The others are ignored, but have
-     * to be evaluated for the sake of side-effects.
-     */
-    public static EvalNode create(JSContext context, Object env, JavaScriptNode sourceArg, JavaScriptNode[] otherArgs, JavaScriptNode functionNode, JavaScriptNode thisObject) {
-        return EvalNodeGen.create(context, env, functionNode, sourceArg, thisObject, otherArgs.length > 0 ? JSFunctionArgumentsNode.create(context, otherArgs) : null);
-    }
-
     @Override
     protected JavaScriptNode copyUninitialized() {
-        return EvalNodeGen.create(context, currEnv, cloneUninitialized(functionNode), cloneUninitialized(sourceNode), cloneUninitialized(thisObject),
-                        AbstractFunctionArgumentsNode.cloneUninitialized(otherArguments));
+        return EvalNodeGen.create(context, cloneUninitialized(functionNode), AbstractFunctionArgumentsNode.cloneUninitialized(arguments), directEvalNode.copyUninitialized());
     }
+
+    protected abstract static class DirectEvalNode extends JavaScriptBaseNode {
+        private final JSContext context;
+        private final Object currEnv;
+        private final JavaScriptNode thisNode;
+
+        protected DirectEvalNode(JSContext context, Object currEnv, JavaScriptNode thisNode) {
+            assert currEnv != null;
+            this.context = context;
+            this.currEnv = currEnv;
+            this.thisNode = thisNode;
+        }
+
+        protected static DirectEvalNode create(JSContext context, Object currEnv, JavaScriptNode thisNode) {
+            return EvalNodeGen.DirectEvalNodeGen.create(context, currEnv, thisNode);
+        }
+
+        public abstract Object executeWithSource(VirtualFrame frame, Object source);
+
+        @Specialization
+        protected int directEvalInt(int source) {
+            return source;
+        }
+
+        @Specialization
+        protected LargeInteger directEvalLargeInteger(LargeInteger source) {
+            return source;
+        }
+
+        @Specialization
+        protected long directEvalLong(long source) {
+            return source;
+        }
+
+        @Specialization
+        protected double directEvalDouble(double source) {
+            return source;
+        }
+
+        @Specialization
+        protected boolean directEvalBoolean(boolean source) {
+            return source;
+        }
+
+        @Specialization
+        protected Symbol directEvalSymbol(Symbol source) {
+            return source;
+        }
+
+        @Specialization
+        protected BigInt directEvalBigInt(BigInt source) {
+            return source;
+        }
+
+        @Specialization(guards = {"isJSType(source)"})
+        protected DynamicObject directEvalJSType(DynamicObject source) {
+            return source;
+        }
+
+        @Specialization
+        protected Object directEvalCharSequence(VirtualFrame frame, CharSequence source) {
+            return directEvalImpl(frame, source);
+        }
+
+        private Object directEvalImpl(VirtualFrame frame, CharSequence sourceCode) {
+            final Source source = sourceFromString(sourceCode);
+            return context.getEvaluator().evaluate(context.getRealm(), getParent(), source, currEnv, frame.materialize(), thisNode.execute(frame));
+        }
+
+        @Specialization(guards = {"isForeignObject(sourceCode)"}, limit = "3")
+        protected Object directEvalForeignObject(VirtualFrame frame, TruffleObject sourceCode,
+                        @CachedLibrary("sourceCode") InteropLibrary interop) {
+            if (interop.isString(sourceCode)) {
+                try {
+                    return directEvalImpl(frame, interop.asString(sourceCode));
+                } catch (UnsupportedMessageException ex) {
+                    throw Errors.createTypeErrorInteropException(sourceCode, ex, "asString", this);
+                }
+            } else {
+                return sourceCode;
+            }
+        }
+
+        @TruffleBoundary
+        private Source sourceFromString(CharSequence sourceCode) {
+            String evalSourceName = null;
+            if (context.isOptionV8CompatibilityMode()) {
+                evalSourceName = formatEvalOrigin(this);
+            }
+            if (evalSourceName == null) {
+                evalSourceName = Evaluator.EVAL_SOURCE_NAME;
+            }
+            return Source.newBuilder(JavaScriptLanguage.ID, sourceCode.toString(), evalSourceName).build();
+        }
+
+        protected DirectEvalNode copyUninitialized() {
+            return DirectEvalNode.create(context, currEnv, cloneUninitialized(thisNode));
+        }
+
+    }
+
 }
