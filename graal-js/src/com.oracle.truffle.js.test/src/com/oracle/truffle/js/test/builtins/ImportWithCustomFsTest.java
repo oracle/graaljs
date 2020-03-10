16a8dd9efcd487d6991767a497484f670b3cf704
Daniele Bonetta <daniele.bonetta@oracle.com>
2020-03-10 15:36:04 -0400

    Handle URIs in the Truffle filesystem.

---
+++ graal-js/src/com.oracle.truffle.js.test/src/com/oracle/truffle/js/test/builtins/ImportWithCustomFsTest.java
@@ -0,0 +1,191 @@
+/*
+ * Copyright (c) 2020, 2020, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * The Universal Permissive License (UPL), Version 1.0
+ *
+ * Subject to the condition set forth below, permission is hereby granted to any
+ * person obtaining a copy of this software, associated documentation and/or
+ * data (collectively the "Software"), free of charge and under any and all
+ * copyright rights in the Software, and any and all patent rights owned or
+ * freely licensable by each licensor hereunder covering either (i) the
+ * unmodified Software as contributed to or provided by such licensor, or (ii)
+ * the Larger Works (as defined below), to deal in both
+ *
+ * (a) the Software, and
+ *
+ * (b) any piece of software and/or hardware listed in the lrgrwrks.txt file if
+ * one is included with the Software each a "Larger Work" to which the Software
+ * is contributed by such licensors),
+ *
+ * without restriction, including without limitation the rights to copy, create
+ * derivative works of, display, perform, and distribute the Software and make,
+ * use, sell, offer for sale, import, export, have made, and have sold the
+ * Software and the Larger Work(s), and to sublicense the foregoing rights on
+ * either these or other terms.
+ *
+ * This license is subject to the following condition:
+ *
+ * The above copyright notice and either this complete permission notice or at a
+ * minimum a reference to the UPL must be included in all copies or substantial
+ * portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.oracle.truffle.js.test.builtins;
+
+import com.oracle.truffle.js.test.JSTest;
+import org.graalvm.polyglot.Context;
+import org.graalvm.polyglot.PolyglotAccess;
+import org.graalvm.polyglot.Source;
+import org.graalvm.polyglot.Value;
+import org.graalvm.polyglot.io.FileSystem;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.net.URI;
+import java.nio.channels.SeekableByteChannel;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.AccessMode;
+import java.nio.file.DirectoryStream;
+import java.nio.file.LinkOption;
+import java.nio.file.OpenOption;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.FileAttribute;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import static com.oracle.truffle.js.lang.JavaScriptLanguage.ID;
+
+public class ImportWithCustomFsTest {
+
+    @Test
+    public void testHttp() throws IOException {
+        final String moduleBody = "export const foo = 41;";
+        final String expectedSpecifier = "https://unpkg.com/@esm/ms";
+        final String testSrc = "import {foo} from 'https://unpkg.com/@esm/ms'; foo;";
+        TestFS fs = new TestFS(expectedSpecifier, moduleBody);
+        Value v = assertFsLoads(fs, testSrc);
+        Assert.assertEquals(41, v.asInt());
+        Assert.assertTrue(fs.uriSpecifiers.contains(expectedSpecifier));
+        Assert.assertFalse(fs.stringSpecifiers.contains(expectedSpecifier));
+    }
+
+    @Test
+    public void testFile() throws IOException {
+        final String moduleBody = "export const foo = 42;";
+        final String expectedSpecifier = "file://path-to-something";
+        final String testSrc = "import {foo} from 'file://path-to-something'; foo;";
+        TestFS fs = new TestFS(expectedSpecifier, moduleBody);
+        Value v = assertFsLoads(fs, testSrc);
+        Assert.assertEquals(42, v.asInt());
+        Assert.assertTrue(fs.uriSpecifiers.contains(expectedSpecifier));
+        Assert.assertFalse(fs.stringSpecifiers.contains(expectedSpecifier));
+    }
+
+    @Test
+    public void testPath() throws IOException {
+        final String expectedSpecifier = "/path";
+        final String moduleBody = "export const foo = 43;";
+        final String testSrc = "import {foo} from '/path'; foo;";
+        TestFS fs = new TestFS(expectedSpecifier, moduleBody);
+        Value v = assertFsLoads(fs, testSrc);
+        Assert.assertEquals(43, v.asInt());
+        Assert.assertTrue(fs.stringSpecifiers.contains(expectedSpecifier));
+        Assert.assertFalse(fs.uriSpecifiers.contains(expectedSpecifier));
+    }
+
+    private static Value assertFsLoads(TestFS fs, String testSrc) throws IOException {
+        Context cx = JSTest.newContextBuilder().allowPolyglotAccess(PolyglotAccess.ALL).allowIO(true).fileSystem(fs).build();
+        return cx.eval(Source.newBuilder(ID, testSrc, "test.mjs").build());
+    }
+
+    private static class TestFS implements FileSystem {
+
+        private final Path dummyPath;
+        private final String moduleBody;
+        private final String expectedPath;
+        private final Set<String> uriSpecifiers;
+        private final Set<String> stringSpecifiers;
+
+        TestFS(String expectedPath, String moduleBody) {
+            this.expectedPath = expectedPath;
+            this.moduleBody = moduleBody;
+            this.dummyPath = Paths.get("/dummy");
+            this.uriSpecifiers = new HashSet<>();
+            this.stringSpecifiers = new HashSet<>();
+        }
+
+        @Override
+        public Path parsePath(URI uri) {
+            uriSpecifiers.add(uri.toString());
+            if (expectedPath.equals(uri.toString())) {
+                return dummyPath;
+            } else {
+                return Paths.get(uri);
+            }
+        }
+
+        @Override
+        public Path parsePath(String path) {
+            stringSpecifiers.add(path);
+            if (expectedPath.equals(path)) {
+                return dummyPath;
+            } else {
+                return Paths.get(path);
+            }
+        }
+
+        @Override
+        public void checkAccess(Path path, Set<? extends AccessMode> modes, LinkOption... linkOptions) {
+        }
+
+        @Override
+        public void createDirectory(Path dir, FileAttribute<?>... attrs) {
+            throw new AssertionError();
+        }
+
+        @Override
+        public void delete(Path path) {
+            throw new AssertionError();
+        }
+
+        @Override
+        public SeekableByteChannel newByteChannel(Path path, Set<? extends OpenOption> options, FileAttribute<?>... attrs) {
+            if (dummyPath.equals(path)) {
+                return new ReadOnlySeekableByteArrayChannel(moduleBody.getBytes(StandardCharsets.UTF_8));
+            } else {
+                throw new AssertionError();
+            }
+        }
+
+        @Override
+        public DirectoryStream<Path> newDirectoryStream(Path dir, DirectoryStream.Filter<? super Path> filter) {
+            throw new AssertionError();
+        }
+
+        @Override
+        public Path toAbsolutePath(Path path) {
+            return path.toAbsolutePath();
+        }
+
+        @Override
+        public Path toRealPath(Path path, LinkOption... linkOptions) {
+            return path;
+        }
+
+        @Override
+        public Map<String, Object> readAttributes(Path path, String attributes, LinkOption... options) {
+            throw new AssertionError();
+        }
+    }
+}
