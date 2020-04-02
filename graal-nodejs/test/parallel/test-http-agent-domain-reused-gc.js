ac6ab33750d357f780d104a78e56f929864c5fc3
Taha BASRI <taha.basri@oracle.com>
2020-04-02 12:25:21 +0100

    Removing test code requiring Class fields

--- graal-nodejs/test/parallel/test-http-agent-domain-reused-gc.js
+++ graal-nodejs/test/parallel/test-http-agent-domain-reused-gc.js
@@ -55,7 +55,10 @@ server.emit('connection', serverSide);
 
 // HTTP Agent that only returns the fake connection.
 class TestAgent extends http.Agent {
-  createConnection = common.mustCall(() => wrappedClientSide)
+  constructor(options) {
+    super(options);
+    this.createConnection = common.mustCall(() => wrappedClientSide)
+  }
 }
 const agent = new TestAgent({ keepAlive: true, maxSockets: 1 });
 
