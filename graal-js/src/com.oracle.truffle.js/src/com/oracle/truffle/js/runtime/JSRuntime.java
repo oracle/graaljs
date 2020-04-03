78c4986309d21676b1827fa40051c27859134cf5
Christian Wirth <christian.wirth@oracle.com>
2020-04-03 14:32:09 +0200

    simplify JSRuntime.equal

--- graal-js/src/com.oracle.truffle.js/src/com/oracle/truffle/js/runtime/JSRuntime.java
+++ graal-js/src/com.oracle.truffle.js/src/com/oracle/truffle/js/runtime/JSRuntime.java
@@ -1497,14 +1497,10 @@ public final class JSRuntime {
         } else if (b instanceof Boolean) {
             return equal(a, booleanToNumber((Boolean) b));
         } else if (isObject(a)) {
-            if (b == Undefined.instance || b == Null.instance) {
-                return false;
-            }
+            assert b != Undefined.instance && b != Null.instance; // covered by (DynOb, DynOb)
             return equal(JSObject.toPrimitive((DynamicObject) a), b);
         } else if (isObject(b)) {
-            if (a == Undefined.instance || a == Null.instance) {
-                return false;
-            }
+            assert b != Undefined.instance && b != Null.instance; // covered by (DynOb, DynOb)
             return equal(a, JSObject.toPrimitive(((DynamicObject) b)));
         } else if (isForeignObject(a) || isForeignObject(b)) {
             return equalInterop(a, b);
