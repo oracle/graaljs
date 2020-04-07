021f5fed9cdc22d98d7be99c4332c4dfc1c345c5
Christian Wirth <christian.wirth@oracle.com>
2020-04-07 17:12:16 +0200

    add test for JSToObjectArray

---
+++ graal-js/src/com.oracle.truffle.js.test/js/runtime/jsToObjectArray.js
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2020, 2020, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * Licensed under the Universal Permissive License v 1.0 as shown at http://oss.oracle.com/licenses/upl.
+ */
+/*
+ * Test otherwise untested JSToObjectArray node.
+ */
+ 
+load('../assert.js');
+
+function fn() { return arguments; }
+var obj = {};
+
+function testReflect(value) {
+    assertFail( ()=>{Reflect.apply(fn, obj, value);}, "is not an Object");
+}
+
+testReflect(true);
+testReflect(42);
+testReflect(42.5);
+testReflect("test");
+
+true;
