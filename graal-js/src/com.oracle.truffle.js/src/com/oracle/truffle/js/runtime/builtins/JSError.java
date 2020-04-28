7c80166b4e3cb9d65e8ca943c734576e0f8d4074
Andreas Woess <andreas.woess@oracle.com>
2020-04-29 01:03:00 +0200

    Do not swallow errors from Error.prepareStackTrace.

--- graal-js/src/com.oracle.truffle.js/src/com/oracle/truffle/js/runtime/builtins/JSError.java
+++ graal-js/src/com.oracle.truffle.js/src/com/oracle/truffle/js/runtime/builtins/JSError.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, 2019, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2018, 2020, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * The Universal Permissive License (UPL), Version 1.0
@@ -322,11 +322,7 @@ public final class JSError extends JSBuiltinObject {
     }
 
     private static Object prepareStackWithUserFunction(JSRealm realm, DynamicObject prepareStackTraceFun, DynamicObject errorObj, JSStackTraceElement[] stackTrace) {
-        try {
-            return JSFunction.call(prepareStackTraceFun, errorObj, new Object[]{errorObj, toStructuredStackTrace(realm, stackTrace)});
-        } catch (Exception ex) {
-            return formatStackTrace(stackTrace, errorObj, realm);
-        }
+        return JSFunction.call(prepareStackTraceFun, errorObj, new Object[]{errorObj, toStructuredStackTrace(realm, stackTrace)});
     }
 
     private static DynamicObject toStructuredStackTrace(JSRealm realm, JSStackTraceElement[] stackTrace) {
