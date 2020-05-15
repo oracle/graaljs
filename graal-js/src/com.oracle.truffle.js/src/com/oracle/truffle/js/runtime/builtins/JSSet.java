d46dd5cbbce414a947533e0b81df443db843a321
SergiyIvan <serhii.ivanenko@oracle.com>
2020-05-15 17:40:39 +0300

    Removed redundant preventExtensions, added tests

--- graal-js/src/com.oracle.truffle.js/src/com/oracle/truffle/js/runtime/builtins/JSSet.java
+++ graal-js/src/com.oracle.truffle.js/src/com/oracle/truffle/js/runtime/builtins/JSSet.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, 2019, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2018, 2020, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * The Universal Permissive License (UPL), Version 1.0
@@ -146,7 +146,6 @@ public final class JSSet extends JSBuiltinObject implements JSConstructorFactory
             return JSFunctionData.createCallOnly(c, callTarget, 0, "get " + SIZE);
         });
         DynamicObject sizeGetter = JSFunction.create(realm, getterData);
-        JSObject.preventExtensions(sizeGetter);
         return sizeGetter;
     }
 
