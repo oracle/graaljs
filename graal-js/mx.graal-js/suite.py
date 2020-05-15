299f61f58255b89add37b92882f1da7ffdf76fc4
Gilles Duboscq <gilles.m.duboscq@oracle.com>
2020-05-15 11:45:03 +0200

    GraalVM 20.1.0

--- graal-js/mx.graal-js/suite.py
+++ graal-js/mx.graal-js/suite.py
@@ -4,7 +4,7 @@ suite = {
   "name" : "graal-js",
 
   "version" : "20.1.0",
-  "release" : False,
+  "release" : True,
   "groupId" : "org.graalvm.js",
   "url" : "http://www.graalvm.org/",
   "developer" : {
