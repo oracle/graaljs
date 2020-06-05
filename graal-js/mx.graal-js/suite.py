70699ab90e52d40cd2586abcceb2a9d39d57fc3c
Gilles Duboscq <gilles.m.duboscq@oracle.com>
2020-06-05 15:54:37 +0200

    Next dev cycle

--- graal-js/mx.graal-js/suite.py
+++ graal-js/mx.graal-js/suite.py
@@ -3,8 +3,8 @@ suite = {
 
   "name" : "graal-js",
 
-  "version" : "20.1.0",
-  "release" : True,
+  "version" : "20.1.1",
+  "release" : False,
   "groupId" : "org.graalvm.js",
   "url" : "http://www.graalvm.org/",
   "developer" : {
