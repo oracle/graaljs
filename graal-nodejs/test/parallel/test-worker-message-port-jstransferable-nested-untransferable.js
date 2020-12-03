064b2bae48b5de3670d9867899690f03287cfac1
Jan Stola <jan.stola@oracle.com>
2020-12-03 18:00:58 +0100

    Replacing the usage of the unsupported class fields in test-worker-message-port-jstransferable-nested-untransferable.

--- graal-nodejs/test/parallel/test-worker-message-port-jstransferable-nested-untransferable.js
+++ graal-nodejs/test/parallel/test-worker-message-port-jstransferable-nested-untransferable.js
@@ -17,18 +17,18 @@ class OuterTransferable extends JSTransferable {
     const c = new MessageChannel();
     this.inner = c.port1;
     c.port2.postMessage(this.inner, [ this.inner ]);
-  }
 
-  [kTransferList] = common.mustCall(() => {
+  this[kTransferList] = common.mustCall(() => {
     return [ this.inner ];
   });
 
-  [kTransfer] = common.mustCall(() => {
+  this[kTransfer] = common.mustCall(() => {
     return {
       data: { inner: this.inner },
       deserializeInfo: 'does-not:matter'
     };
   });
+  }
 }
 
 const { port1 } = new MessageChannel();
