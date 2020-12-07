a6ed1a66dc6b1ee49d1695d32bc8af17b5ed07c0
Korti <lucas.korthauer@gmail.com>
2020-12-07 17:06:26 +0100

    Added access properties years, months, weeks, days, hours, minutes, seconds, milliseconds, microseconds and nanoseconds for Temporal.Duration.

--- graal-js/src/com.oracle.truffle.js/src/com/oracle/truffle/js/runtime/Errors.java
+++ graal-js/src/com.oracle.truffle.js/src/com/oracle/truffle/js/runtime/Errors.java
@@ -549,6 +549,10 @@ public final class Errors {
         return Errors.createTypeError("Temporal.Time expected");
     }
 
+    public static JSException createTypeErrorTemporalDurationExpected() {
+        return Errors.createTypeError("Temporal.Duration expected");
+    }
+
     @TruffleBoundary
     public static JSException createTypeErrorCallableExpected() {
         return Errors.createTypeError("Callable expected");
