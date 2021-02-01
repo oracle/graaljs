afd386ac959a22a68a7bbad16e898990ff4d0d63
Andreas Woess <andreas.woess@oracle.com>
2021-02-01 17:00:12 +0100

    Add ToPropertyKey fast path for symbols keys.

--- graal-js/src/com.oracle.truffle.js/src/com/oracle/truffle/js/runtime/JSRuntime.java
+++ graal-js/src/com.oracle.truffle.js/src/com/oracle/truffle/js/runtime/JSRuntime.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, 2020, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2018, 2021, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * The Universal Permissive License (UPL), Version 1.0
@@ -2474,6 +2474,8 @@ public final class JSRuntime {
     @TruffleBoundary
     public static Object toPropertyKey(Object arg) {
         if (arg instanceof String) {
+            return arg;
+        } else if (arg instanceof Symbol) {
             return arg;
         }
         Object key = toPrimitive(arg);
