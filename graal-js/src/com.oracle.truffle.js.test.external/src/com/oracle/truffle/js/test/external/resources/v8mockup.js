75441fe5a52057e86cc7522d73b259ba98d021b3
Christian Wirth <christian.wirth@oracle.com>
2021-02-15 18:38:57 +0100

    update TestV8

--- graal-js/src/com.oracle.truffle.js.test.external/src/com/oracle/truffle/js/test/external/resources/v8mockup.js
+++ graal-js/src/com.oracle.truffle.js.test.external/src/com/oracle/truffle/js/test/external/resources/v8mockup.js
@@ -972,3 +972,61 @@ function v8ReferenceEqual(a, b) {
 
 function v8CollectTypeProfile() {
 }
+
+function v8CompileBaseline() {
+}
+
+function v8IsDictPropertyConstTrackingEnabled() {
+	return v8IgnoreResult;
+}
+
+function v8HasOwnConstDataProperty() {
+	return v8IgnoreResult;
+}
+
+function v8RegexpIsUnmodified() {
+	return v8IgnoreResult;
+}
+
+function v8PromiseSpeciesProtector() {
+	return v8IgnoreResult;
+}
+
+function v8RegExpSpeciesProtector() {
+	return v8IgnoreResult;
+}
+
+function v8TypedArraySpeciesProtector() {
+	return v8IgnoreResult;
+}
+
+function v8ArrayIteratorProtector() {
+	return v8IgnoreResult;
+}
+
+function v8ToString(a) {
+    return TestV8.toStringConv(a);
+}
+
+function v8ScheduleGCInStackCheck(){
+}
+
+function v8DynamicCheckMapsEnabled() {
+	return v8IgnoreResult;
+}
+
+function v8TierupFunctionOnNextCall() {
+}
+
+function v8IsTopTierTurboprop() {
+}
+
+function v8WasmTierUp() {
+}
+
+function v8WasmTierDown() {
+}
+
+function v8RegexpTypeTag() {
+	return v8IgnoreResult;
+}
