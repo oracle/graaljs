ef3b32e81a94a023aa5ac65d834c61cb2cab59d0
Andreas Woess <andreas.woess@oracle.com>
2021-03-19 00:39:19 +0100

    Update foreign hash map test.

--- graal-js/src/com.oracle.truffle.js.test/js/interop/foreign_map_method_vs_entry.js
+++ graal-js/src/com.oracle.truffle.js.test/js/interop/foreign_map_method_vs_entry.js
@@ -5,19 +5,40 @@
  * Licensed under the Universal Permissive License v 1.0 as shown at http://oss.oracle.com/licenses/upl.
  */
 
+/*
+ * Tests foreign map method vs. hash entry precedence.
+ *
+ * @option foreign-object-prototype
+ */
+
 load('../assert.js');
 
 const HashMap = Java.type("java.util.LinkedHashMap");
 
-let functionValue = () => {fail("should not be called");}
+let doNotCallMe = () => {fail("should not be called");};
+let callMeMaybe = () => 'ok';
 
 let foreignMap = new HashMap();
 foreignMap.put("size", 42);
-foreignMap.put("clear", functionValue);
+foreignMap.put("clear", doNotCallMe);
+foreignMap.put("callMeMaybe", callMeMaybe);
+foreignMap.put("set", doNotCallMe);
+foreignMap.put("notCallable", 42);
 
+// invocation favors methods over hash entries.
 assertSame(42, foreignMap.size);
-assertSame(2, foreignMap.size());
+assertSame(5, foreignMap.size());
 
-assertSame(functionValue, foreignMap.clear);
+// invocation favors foreign object prototype methods over hash entries, too.
+foreignMap.set("set", callMeMaybe);
+assertSame(callMeMaybe, foreignMap.set);
+
+assertThrows(() => foreignMap.doesNotExist(), TypeError);
+assertThrows(() => foreignMap.notCallable(), TypeError);
+
+// if the hash entry is callable and there's no member with the same name, we should be able to invoke it.
+assertSame('ok', foreignMap.callMeMaybe());
+
+assertSame(doNotCallMe, foreignMap.clear);
 foreignMap.clear();
 assertSame(0, foreignMap.size());
