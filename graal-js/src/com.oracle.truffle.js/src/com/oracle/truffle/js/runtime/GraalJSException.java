bc60481c572ffb14946b0e963ef0982be213da02
Andreas Woess <andreas.woess@oracle.com>
2021-04-21 15:11:41 +0200

    Limit the number of captured Truffle stack trace elements.

--- graal-js/src/com.oracle.truffle.js/src/com/oracle/truffle/js/runtime/GraalJSException.java
+++ graal-js/src/com.oracle.truffle.js/src/com/oracle/truffle/js/runtime/GraalJSException.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, 2020, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2018, 2021, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * The Universal Permissive License (UPL), Version 1.0
@@ -112,7 +112,7 @@ public abstract class GraalJSException extends AbstractTruffleException {
     }
 
     private static int truffleStackTraceLimit(int stackTraceLimit) {
-        return stackTraceLimit <= 0 ? 0 : UNLIMITED_STACK_TRACE;
+        return stackTraceLimit <= 0 ? 0 : stackTraceLimit;
     }
 
     protected static <T extends GraalJSException> T fillInStackTrace(T exception, DynamicObject skipFramesUpTo, boolean capture) {
