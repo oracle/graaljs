e47b38ed976a03cb7f4c3a8b55ee8433c3511be7
Jan Stola <jan.stola@oracle.com>
2021-05-07 12:10:04 +0200

    Enabling a test of a non-configurable property of a sandbox.

--- graal-nodejs/test/graal/unit/vm.js
+++ graal-nodejs/test/graal/unit/vm.js
@@ -75,12 +75,6 @@ describe('vm', function () {
         assert.strictEqual(desc.value, value);
         assert.strictEqual(desc.writable, false);
         assert.strictEqual(desc.enumerable, false);
-        // The following assertion holds on the original Node.js but we
-        // fail to satisfy it because the global object of the context
-        // is implemented by Proxy that cannot have non-configurable
-        // properties not present in the target (which is the original
-        // global object, not the sandbox - where the non-configurable
-        // property is defined)
-        //assert.strictEqual(desc.configurable, false);
+        assert.strictEqual(desc.configurable, false);
     });
 });
