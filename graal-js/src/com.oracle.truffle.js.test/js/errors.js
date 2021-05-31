80c3882c6560085932b3d7942deb9469904db1cf
Florian Huemer <fhuemer@live.at>
2021-05-31 15:39:55 +0200

    Added additional JS tests that deal with throwing Errors.

---
+++ graal-js/src/com.oracle.truffle.js.test/js/errors.js
@@ -0,0 +1,162 @@
+/*
+ * Copyright (c) 2020, 2020, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * Licensed under the Universal Permissive License v 1.0 as shown at http://oss.oracle.com/licenses/upl.
+ */
+
+/**
+ * Test for checking the order of properties in AggregateError.
+ *
+ * @option ecmascript-version=2022
+ */
+
+load('assert.js');
+
+try {
+    throw new Error('message', { cause: 'test'});
+} catch(e) {
+    assertSame('message', e.message);
+    assertSame('test', e.cause);
+}
+
+try {
+    throw new Error('message');
+} catch(e) {
+    assertSame('message', e.message);
+    assertSame(undefined, e.cause);
+}
+
+try {
+    throw new RangeError('message', { cause: 'test'});
+} catch(e) {
+    assertSame('message', e.message);
+    assertSame('test', e.cause);
+}
+
+try {
+    throw new RangeError('message');
+} catch(e) {
+    assertSame('message', e.message);
+    assertSame(undefined, e.cause);
+}
+
+try {
+    throw new TypeError('message', { cause: 'test'});
+} catch(e) {
+    assertSame('message', e.message);
+    assertSame('test', e.cause);
+}
+
+try {
+    throw new TypeError('message');
+} catch(e) {
+    assertSame('message', e.message);
+    assertSame(undefined, e.cause);
+}
+
+try {
+    throw new ReferenceError('message', { cause: 'test'});
+} catch(e) {
+    assertSame('message', e.message);
+    assertSame('test', e.cause);
+}
+
+try {
+    throw new ReferenceError('message');
+} catch(e) {
+    assertSame('message', e.message);
+    assertSame(undefined, e.cause);
+}
+
+try {
+    throw new SyntaxError('message', { cause: 'test'});
+} catch(e) {
+    assertSame('message', e.message);
+    assertSame('test', e.cause);
+}
+
+try {
+    throw new SyntaxError('message');
+} catch(e) {
+    assertSame('message', e.message);
+    assertSame(undefined, e.cause);
+}
+
+try {
+    throw new EvalError('message', { cause: 'test'});
+} catch(e) {
+    assertSame('message', e.message);
+    assertSame('test', e.cause);
+}
+
+try {
+    throw new EvalError('message');
+} catch(e) {
+    assertSame('message', e.message);
+    assertSame(undefined, e.cause);
+}
+
+try {
+    throw new URIError('message', { cause: 'test'});
+} catch(e) {
+    assertSame('message', e.message);
+    assertSame('test', e.cause);
+}
+
+try {
+    throw new URIError('message');
+} catch(e) {
+    assertSame('message', e.message);
+    assertSame(undefined, e.cause);
+}
+
+try {
+    throw new AggregateError([],'message', { cause: 'test'});
+} catch(e) {
+    assertSame('message', e.message);
+    assertSame('test', e.cause);
+}
+
+try {
+    throw new AggregateError([new Error('message1')], 'message2', { cause: 'test'});
+} catch(e) {
+    assertSame('message2', e.message);
+    assertSame('test', e.cause);
+    assertSame('message1', e.errors[0].message);
+    assertSame(undefined, e.errors[0].cause);
+}
+
+try {
+    throw new AggregateError([new Error('message1', { cause: 'test1'})], 'message2', { cause: 'test2'});
+} catch(e) {
+    assertSame('message2', e.message);
+    assertSame('test2', e.cause);
+    assertSame('message1', e.errors[0].message);
+    assertSame('test1', e.errors[0].cause);
+}
+
+try {
+    throw new AggregateError([],'message');
+} catch(e) {
+    assertSame('message', e.message);
+    assertSame(undefined, e.cause);
+}
+
+try {
+    throw new AggregateError([new Error('message1')], 'message2');
+} catch(e) {
+    assertSame('message2', e.message);
+    assertSame(undefined, e.cause);
+    assertSame('message1', e.errors[0].message);
+    assertSame(undefined, e.errors[0].cause);
+}
+
+try {
+    throw new AggregateError([new Error('message1', { cause: 'test1'})], 'message2');
+} catch(e) {
+    assertSame('message2', e.message);
+    assertSame(undefined, e.cause);
+    assertSame('message1', e.errors[0].message);
+    assertSame('test1', e.errors[0].cause);
+}
