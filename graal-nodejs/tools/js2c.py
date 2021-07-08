619249e53923f7d60cd508ae64cdd1265fc1cf84
Jan Stola <jan.stola@oracle.com>
2021-07-08 20:27:38 +0200

    Fixing Windows ninja build of node_javascript.cc.

--- graal-nodejs/tools/js2c.py
+++ graal-nodejs/tools/js2c.py
@@ -123,7 +123,7 @@ def AddModule(filename, definitions, initializers):
   initializers.append(initializer)
 
 def NormalizeFileName(filename):
-  split = filename.split('/')
+  split = filename.split(os.path.sep)
   if split[0] == 'deps':
     split = ['internal'] + split
   else:  # `lib/**/*.js` so drop the 'lib' part
