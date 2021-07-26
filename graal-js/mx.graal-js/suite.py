61dc0a2ecd8173c50f2d242751d03dfa2cd0f325
Taha BASRI <taha.basri@oracle.com>
2021-07-26 23:54:47 +0100

    New dev cycle (20.3.4).

--- graal-js/mx.graal-js/suite.py
+++ graal-js/mx.graal-js/suite.py
@@ -3,8 +3,8 @@ suite = {
 
   "name" : "graal-js",
 
-  "version" : "20.3.3",
-  "release" : True,
+  "version" : "20.3.4",
+  "release" : False,
   "groupId" : "org.graalvm.js",
   "url" : "http://www.graalvm.org/",
   "developer" : {
