259080e23b93ecfffcff4b0c624e6ec9809a0eb6
Andreas Woess <andreas.woess@oracle.com>
2021-09-07 17:52:30 +0200

    Fix: --use-idle-notification is not ignored.

--- graal-nodejs/mx.graal-nodejs/com.oracle.truffle.trufflenode/src/com/oracle/truffle/trufflenode/Options.java
+++ graal-nodejs/mx.graal-nodejs/com.oracle.truffle.trufflenode/src/com/oracle/truffle/trufflenode/Options.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, 2019, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2018, 2021, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * The Universal Permissive License (UPL), Version 1.0
@@ -141,7 +141,7 @@ public final class Options {
                         "nolazy",
                         "nouse-idle-notification",
                         "stack-size",
-                        "use_idle_notification"
+                        "use-idle-notification"
         }));
 
         @Override
