3bcb4377cef425027c302354eb32f1ea6bd3f84f
Jan Stola <jan.stola@oracle.com>
2021-10-07 13:48:55 +0200

    Avoid NPE from NativeFunctionRootNode.getName() during CallTarget creation.

--- graal-nodejs/mx.graal-nodejs/com.oracle.truffle.trufflenode/src/com/oracle/truffle/trufflenode/GraalJSAccess.java
+++ graal-nodejs/mx.graal-nodejs/com.oracle.truffle.trufflenode/src/com/oracle/truffle/trufflenode/GraalJSAccess.java
@@ -1752,6 +1752,7 @@ public final class GraalJSAccess {
         if (functionData == null) {
             JSOrdinary instanceLayout = template.getInstanceTemplate().getInternalFieldCount() > 0 ? JSOrdinary.INTERNAL_FIELD_INSTANCE : JSOrdinary.INSTANCE;
             functionData = JSFunctionData.create(context, template.getLength(), template.getClassName(), template.getPrototypeTemplate() != null, false, false, false);
+            template.setFunctionData(functionData);
             CallTarget callTarget = Truffle.getRuntime().createCallTarget(new ExecuteNativeFunctionNode.NativeFunctionRootNode(this, context, template, false, false));
             CallTarget newCallTarget = Truffle.getRuntime().createCallTarget(new ExecuteNativeFunctionNode.NativeFunctionRootNode(this, context, template, true, false));
             CallTarget newTargetCallTarget = Truffle.getRuntime().createCallTarget(new ExecuteNativeFunctionNode.NativeFunctionRootNode(this, context, template, true, true));
@@ -1760,7 +1761,6 @@ public final class GraalJSAccess {
             functionData.setCallTarget(callTarget);
             functionData.setConstructTarget(constructTarget);
             functionData.setConstructNewTarget(constructNewTarget);
-            template.setFunctionData(functionData);
         }
 
         DynamicObject functionObject = JSFunction.create(realm, functionData);
