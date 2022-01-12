b02e4aed6eaac1b3a73c9dcd1cc4c031b18abe26
Jan Stola <jan.stola@oracle.com>
2022-01-12 16:26:19 +0100

    Replacing the star import by several specific imports.

--- graal-js/src/com.oracle.truffle.js.test/src/com/oracle/truffle/js/test/builtins/GlobalBuiltinTest.java
+++ graal-js/src/com.oracle.truffle.js.test/src/com/oracle/truffle/js/test/builtins/GlobalBuiltinTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019, 2021, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2019, 2022, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * The Universal Permissive License (UPL), Version 1.0
@@ -44,7 +44,9 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
-import java.io.*;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 
 import org.graalvm.polyglot.Context;
