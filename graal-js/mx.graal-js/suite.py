50d5973101d73582ab17b96c38b05b16dac563fb
Mohaned Ez-zarghili <mohamed.ez.zarghili@oracle.com>
2022-01-14 08:38:45 +0100

    [GR-36356] Release GraalVM 20.3.5.

--- graal-js/mx.graal-js/suite.py
+++ graal-js/mx.graal-js/suite.py
@@ -4,7 +4,7 @@ suite = {
   "name" : "graal-js",
 
   "version" : "20.3.5",
-  "release" : False,
+  "release" : True,
   "groupId" : "org.graalvm.js",
   "url" : "http://www.graalvm.org/",
   "developer" : {
