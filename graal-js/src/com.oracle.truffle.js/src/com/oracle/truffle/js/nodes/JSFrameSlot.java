42d742e6d57f1074de7c3063654de60b8a0e7b34
Andreas Woess <andreas.woess@oracle.com>
2022-02-24 05:08:03 +0100

    Handle negative cached Integer values in FrameSlotFlags.of().

--- graal-js/src/com.oracle.truffle.js/src/com/oracle/truffle/js/nodes/JSFrameSlot.java
+++ graal-js/src/com.oracle.truffle.js/src/com/oracle/truffle/js/nodes/JSFrameSlot.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2021, 2022, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * The Universal Permissive License (UPL), Version 1.0
@@ -150,7 +150,7 @@ public final class JSFrameSlot {
 
         static Integer of(int flags) {
             Integer boxed = Integer.valueOf(flags);
-            if (flags >= 128) {
+            if (flags < -128 || flags > 127) {
                 Integer cached = cachedFlags.get(boxed);
                 if (cached != null) {
                     return cached;
