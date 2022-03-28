58deb3d1256693c57bdde8d396243437bcef678a
Jan Stola <jan.stola@oracle.com>
2022-03-28 17:13:51 +0200

    No need to keep TimezoneICUJDKCompat test disabled anymore.

--- graal-js/src/com.oracle.truffle.js.test/src/com/oracle/truffle/js/test/runtime/TimezoneICUJDKCompat.java
+++ graal-js/src/com.oracle.truffle.js.test/src/com/oracle/truffle/js/test/runtime/TimezoneICUJDKCompat.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021, 2021, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2021, 2022, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * The Universal Permissive License (UPL), Version 1.0
@@ -45,10 +45,8 @@ import java.util.HashSet;
 import java.util.Set;
 
 import org.junit.Assert;
-import org.junit.Ignore;
 import org.junit.Test;
 
-@Ignore
 public class TimezoneICUJDKCompat {
 
     @Test
