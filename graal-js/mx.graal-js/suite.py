95d96950d85ee93a223d9d2a73d31eb9f3d22f05
Andreas Woess <andreas.woess@oracle.com>
2022-05-14 14:31:51 +0200

    Update truffle import.

--- graal-js/mx.graal-js/suite.py
+++ graal-js/mx.graal-js/suite.py
@@ -24,7 +24,7 @@ suite = {
         {
            "name" : "regex",
            "subdir" : True,
-           "version" : "3487e6705a86e77e123e813f6f31b3e24232df4b",
+           "version" : "9b9add1f28cc4acb58c9bb00b0be83555ca76d56",
            "urls" : [
                 {"url" : "https://github.com/oracle/graal.git", "kind" : "git"},
                 {"url" : "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind" : "binary"},
