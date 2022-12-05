eca46dd8699e5a38cbb35476b4595c8ce520539d
Andreas Woess <andreas.woess@oracle.com>
2022-12-05 17:22:22 +0100

    [GR-42807] Add regression test.

---
+++ graal-js/src/com.oracle.truffle.js.test/js/GR-42807.js
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2022, 2022, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * Licensed under the Universal Permissive License v 1.0 as shown at http://oss.oracle.com/licenses/upl.
+ */
+
+var s2 = "";
+var a0 = [];
+var a1 = [];
+var a2 = [];
+var a3 = [];
+var a4 = [];
+var o1 = {};
+for (let i = 0; i < 5; i++) {
+    Array.prototype.shift.apply(a1);
+    a1.splice(NaN, -2044533922.5, a0, s2);
+}
+for (let i = 0; i < 5; i++) {
+    Array.prototype.shift.apply(a2);
+    a2.splice(NaN, -2044533922.5, 3, 4);
+}
+for (let i = 0; i < 5; i++) {
+    Array.prototype.shift.apply(a3);
+    a3.splice(NaN, -2044533922.5, 3.1, 4.15);
+}
+for (let i = 0; i < 5; i++) {
+    Array.prototype.shift.apply(a4);
+    a4.splice(NaN, -2044533922.5, o1, o1);
+}
