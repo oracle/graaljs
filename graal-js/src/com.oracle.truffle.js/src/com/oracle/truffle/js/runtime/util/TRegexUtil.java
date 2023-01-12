ec13a76d3cff809dc424935aa1f011a090500028
Andreas Woess <andreas.woess@oracle.com>
2023-01-12 23:08:42 +0100

    Remove unused node.

--- graal-js/src/com.oracle.truffle.js/src/com/oracle/truffle/js/runtime/util/TRegexUtil.java
+++ graal-js/src/com.oracle.truffle.js/src/com/oracle/truffle/js/runtime/util/TRegexUtil.java
@@ -59,7 +59,6 @@ import com.oracle.truffle.js.runtime.JSContext;
 import com.oracle.truffle.js.runtime.Strings;
 import com.oracle.truffle.js.runtime.objects.Undefined;
 import com.oracle.truffle.js.runtime.util.TRegexUtil.Props.CompiledRegex;
-import com.oracle.truffle.js.runtime.util.TRegexUtilFactory.InteropIsMemberReadableNodeGen;
 import com.oracle.truffle.js.runtime.util.TRegexUtilFactory.InteropReadBooleanMemberNodeGen;
 import com.oracle.truffle.js.runtime.util.TRegexUtilFactory.InteropReadIntMemberNodeGen;
 import com.oracle.truffle.js.runtime.util.TRegexUtilFactory.InteropReadMemberNodeGen;
@@ -126,21 +125,6 @@ public final class TRegexUtil {
         }
 
         public static final int CAPTURE_GROUP_NO_MATCH = -1;
-    }
-
-    @GenerateUncached
-    public abstract static class InteropIsMemberReadableNode extends Node {
-
-        public abstract boolean execute(Object obj, String key);
-
-        @Specialization(limit = NUMBER_OF_REGEX_RESULT_TYPES)
-        static boolean read(Object obj, String key, @CachedLibrary("obj") InteropLibrary objs) {
-            return objs.isMemberReadable(obj, key);
-        }
-
-        public static InteropIsMemberReadableNode create() {
-            return InteropIsMemberReadableNodeGen.create();
-        }
     }
 
     @GenerateUncached
