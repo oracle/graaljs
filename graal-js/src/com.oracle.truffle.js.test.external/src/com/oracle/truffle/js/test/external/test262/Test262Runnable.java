fe381baf8eb9e2bc6397dfb4eb5e43cd4b058b15
Andreas Woess <andreas.woess@oracle.com>
2023-01-17 21:34:09 +0100

    Test262: Add Array.fromAsync to unsupported features.

--- graal-js/src/com.oracle.truffle.js.test.external/src/com/oracle/truffle/js/test/external/test262/Test262Runnable.java
+++ graal-js/src/com.oracle.truffle.js.test.external/src/com/oracle/truffle/js/test/external/test262/Test262Runnable.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, 2022, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2018, 2023, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * The Universal Permissive License (UPL), Version 1.0
@@ -244,6 +244,7 @@ public class Test262Runnable extends TestRunnable {
                     "top-level-await",
     }));
     private static final Set<String> UNSUPPORTED_FEATURES = new HashSet<>(Arrays.asList(new String[]{
+                    "Array.fromAsync",
                     "Intl.DurationFormat",
                     "IsHTMLDDA",
                     "String.prototype.isWellFormed",
