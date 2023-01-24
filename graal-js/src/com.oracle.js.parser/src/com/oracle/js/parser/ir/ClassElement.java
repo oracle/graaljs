803831a1be16976aa5fa3ce0cac560cdf30e4801
Andreas Woess <andreas.woess@oracle.com>
2023-01-24 16:52:55 +0100

    Add separator between field name and initializer in ClassElement.toString().

--- graal-js/src/com.oracle.js.parser/src/com/oracle/js/parser/ir/ClassElement.java
+++ graal-js/src/com.oracle.js.parser/src/com/oracle/js/parser/ir/ClassElement.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022, 2022, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2022, 2023, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * The Universal Permissive License (UPL), Version 1.0
@@ -343,6 +343,7 @@ public final class ClassElement extends PropertyNode {
         if (isClassField()) {
             toStringKey(sb, printType);
             if (value != null) {
+                sb.append(" = ");
                 value.toString(sb, printType);
             }
         }
