db14df05faf28faddb44067b531fcd0dc4f92542
Andreas Woess <andreas.woess@oracle.com>
2023-03-14 23:10:19 +0100

    Add test for foreign error cause.

--- graal-js/src/com.oracle.truffle.js.test/js/interop/foreign_error_prototype.js
+++ graal-js/src/com.oracle.truffle.js.test/js/interop/foreign_error_prototype.js
@@ -17,15 +17,87 @@ function throwJavaError(message) {
     throw new java.lang.RuntimeException(message);
 }
 
-var message = 'someMessage';
-try {
-    throwJavaError(message);
-    assert.fail('should have thrown');
-} catch (e) {
+function throwJavaErrorWithCause(message, cause) {
+    throw new java.lang.RuntimeException(message, cause);
+}
+
+function assertJavaError(e) {
     assertTrue(Java.isJavaObject(e));
     assertTrue(e instanceof Error);
+    assertTrue(e instanceof java.lang.RuntimeException);
+}
+
+var message = 'someMessage';
+var otherMessage = 'someOtherMessage';
+
+try {
+    throwJavaError(message);
+    fail('should have thrown');
+} catch (e) {
+    assertJavaError(e);
     assertSame(message, e.message);
     assertTrue(e.stack.includes('foreign_error_prototype'));
     assertTrue(e.stack.includes('throwJavaError'));
     assertSame('Error: ' + message, Error.prototype.toString.call(e));
+
+    assertSame(undefined, e.cause);
+}
+
+try {
+    throwJavaError(message);
+    fail('should have thrown');
+} catch (cause) {
+    try {
+        throwJavaErrorWithCause(otherMessage, cause);
+        fail('should have thrown');
+    } catch (e) {
+        assertJavaError(e);
+        assertSame(otherMessage, e.message);
+        assertTrue(e.stack.includes('foreign_error_prototype'));
+        assertTrue(e.stack.includes('throwJavaErrorWithCause'));
+        assertSame('Error: ' + otherMessage, Error.prototype.toString.call(e));
+
+        assertSame(cause, e.cause);
+
+        e = e.cause;
+        assertJavaError(e);
+        assertSame(message, e.message);
+        assertTrue(e.stack.includes('foreign_error_prototype'));
+        assertTrue(e.stack.includes('throwJavaError'));
+        assertSame('Error: ' + message, Error.prototype.toString.call(e));
+
+        assertSame(undefined, e.cause);
+    }
+}
+
+class JSError extends Error {}
+
+function throwJSErrorWithCause(message, cause) {
+    throw new JSError(message, {cause});
+}
+
+try {
+    throwJavaError(message);
+    fail('should have thrown');
+} catch (cause) {
+    try {
+        throwJSErrorWithCause(otherMessage, cause);
+        fail('should have thrown');
+    } catch (e) {
+        assertTrue(e instanceof JSError);
+        assertSame(otherMessage, e.message);
+        assertTrue(e.stack.includes('foreign_error_prototype'));
+        assertTrue(e.stack.includes('throwJSError'));
+
+        assertSame(cause, e.cause);
+
+        e = e.cause;
+        assertJavaError(e);
+        assertSame(message, e.message);
+        assertTrue(e.stack.includes('foreign_error_prototype'));
+        assertTrue(e.stack.includes('throwJavaError'));
+        assertSame('Error: ' + message, Error.prototype.toString.call(e));
+
+        assertSame(undefined, e.cause);
+    }
 }
