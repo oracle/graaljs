023fbed9251e8883e5c8bf21507ccc30b40d0fad
Andreas Woess <andreas.woess@oracle.com>
2023-04-04 17:54:18 +0200

    Extend native property enumerator handler result with target's [[OwnPropertyKeys]].

--- graal-nodejs/test/graal/unit/vm.js
+++ graal-nodejs/test/graal/unit/vm.js
@@ -128,4 +128,32 @@ describe('vm', function () {
         assert.strictEqual(vm.runInContext('typeof global', context), 'undefined');
         assert.strictEqual(vm.runInContext('typeof globalThis.global', context), 'undefined');
     });
+    it('should include global own keys', function () {
+        var emptyContext = vm.createContext();
+        var context = vm.createContext({1: 'one', 3: 'three', extra: 42, [Symbol.unscopables]: 43});
+
+        for (let query of ['Object.getOwnPropertyNames(globalThis)', 'Reflect.ownKeys(globalThis)']) {
+            var globalBuiltins = vm.runInContext(query, emptyContext);
+            assert(globalBuiltins.includes('Object'), globalBuiltins);
+
+            var globalPropertyNames = vm.runInContext('globalThis[0] = "zero"; globalThis[2] = "two"; ' + query, context);
+            assert(globalPropertyNames.includes('Object'), globalPropertyNames);
+
+            // Make sure properties are in the correct order, too.
+            assert.strictEqual(globalPropertyNames[0], '0');
+            assert.strictEqual(globalPropertyNames[1], '1');
+            assert.strictEqual(globalPropertyNames[2], '2');
+            assert.strictEqual(globalPropertyNames[3], '3');
+            assert.strictEqual(globalPropertyNames[4], 'extra');
+            assert.strictEqual(globalPropertyNames[5], globalBuiltins[0]);
+
+            // Symbols of the context object are not included.
+            assert(!globalPropertyNames.includes(Symbol.unscopables), globalPropertyNames);
+        }
+
+        var globalPropertySymbols = vm.runInContext('Object.getOwnPropertySymbols(globalThis)', emptyContext);
+        // Symbols of the context object are not included.
+        assert(!globalPropertySymbols.includes(Symbol.unscopables), globalPropertySymbols);
+        assert.strictEqual(43, vm.runInContext('globalThis[Symbol.unscopables]', context));
+    });
 });
