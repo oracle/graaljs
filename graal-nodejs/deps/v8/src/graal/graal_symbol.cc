bab57622c6bf1ae4c3e584a11c6cd37dacea6fbb
Jan Stola <jan.stola@oracle.com>
2023-05-16 12:01:16 +0200

    Using GraalSymbol::Allocate() instead of GraalSymbol constructor.

--- graal-nodejs/deps/v8/src/graal/graal_symbol.cc
+++ graal-nodejs/deps/v8/src/graal/graal_symbol.cc
@@ -46,7 +46,7 @@
 #include "graal_symbol-inl.h"
 
 GraalHandleContent* GraalSymbol::CopyImpl(jobject java_object_copy) {
-    return new GraalSymbol(Isolate(), java_object_copy);
+    return GraalSymbol::Allocate(Isolate(), java_object_copy);
 }
 
 v8::Local<v8::Symbol> GraalSymbol::New(v8::Isolate* isolate, v8::Local<v8::String> name) {
@@ -54,7 +54,7 @@ v8::Local<v8::Symbol> GraalSymbol::New(v8::Isolate* isolate, v8::Local<v8::Strin
     GraalString* graal_name = reinterpret_cast<GraalString*> (*name);
     jobject java_name = graal_name->GetJavaObject();
     JNI_CALL(jobject, java_symbol, graal_isolate, GraalAccessMethod::symbol_new, Object, java_name);
-    GraalSymbol* graal_symbol = new GraalSymbol(graal_isolate, java_symbol);
+    GraalSymbol* graal_symbol = GraalSymbol::Allocate(graal_isolate, java_symbol);
     return reinterpret_cast<v8::Symbol*> (graal_symbol);
 }
 
@@ -63,7 +63,7 @@ v8::Local<v8::Private> GraalSymbol::NewPrivate(v8::Isolate* isolate, v8::Local<v
     GraalString* graal_description = reinterpret_cast<GraalString*> (*description);
     jobject java_description = graal_description->GetJavaObject();
     JNI_CALL(jobject, java_symbol, graal_isolate, GraalAccessMethod::symbol_private_new, Object, java_description);
-    GraalSymbol* graal_symbol = new GraalSymbol(graal_isolate, java_symbol);
+    GraalSymbol* graal_symbol = GraalSymbol::Allocate(graal_isolate, java_symbol);
     return reinterpret_cast<v8::Private*> (graal_symbol);
 }
 
@@ -71,7 +71,7 @@ v8::Local<v8::Private> GraalSymbol::NewPrivate(v8::Isolate* isolate, v8::Local<v
 v8::Local<v8::Symbol> GraalSymbol::getter(v8::Isolate* isolate) { \
     GraalIsolate* graal_isolate = reinterpret_cast<GraalIsolate*> (isolate); \
     JNI_CALL(jobject, java_symbol, graal_isolate, GraalAccessMethod::method_id, Object); \
-    GraalSymbol* graal_symbol = new GraalSymbol(graal_isolate, java_symbol); \
+    GraalSymbol* graal_symbol = GraalSymbol::Allocate(graal_isolate, java_symbol); \
     return reinterpret_cast<v8::Symbol*> (graal_symbol); \
 }
 
@@ -92,7 +92,7 @@ v8::Local<v8::Symbol> GraalSymbol::For(v8::Isolate* isolate, v8::Local<v8::Strin
     GraalString* graal_description = reinterpret_cast<GraalString*> (*description);
     jobject java_description = graal_description->GetJavaObject();
     JNI_CALL(jobject, java_symbol, graal_isolate, GraalAccessMethod::symbol_for, Object, java_description);
-    GraalSymbol* graal_symbol = new GraalSymbol(graal_isolate, java_symbol);
+    GraalSymbol* graal_symbol = GraalSymbol::Allocate(graal_isolate, java_symbol);
     return reinterpret_cast<v8::Symbol*> (graal_symbol);
 }
 
@@ -101,7 +101,7 @@ v8::Local<v8::Symbol> GraalSymbol::ForApi(v8::Isolate* isolate, v8::Local<v8::St
     GraalString* graal_description = reinterpret_cast<GraalString*> (*description);
     jobject java_description = graal_description->GetJavaObject();
     JNI_CALL(jobject, java_symbol, graal_isolate, GraalAccessMethod::symbol_for_api, Object, java_description);
-    GraalSymbol* graal_symbol = new GraalSymbol(graal_isolate, java_symbol);
+    GraalSymbol* graal_symbol = GraalSymbol::Allocate(graal_isolate, java_symbol);
     return reinterpret_cast<v8::Symbol*> (graal_symbol);
 }
 
@@ -110,7 +110,7 @@ v8::Local<v8::Private> GraalSymbol::PrivateForApi(v8::Isolate* isolate, v8::Loca
     GraalString* graal_description = reinterpret_cast<GraalString*> (*description);
     jobject java_description = graal_description->GetJavaObject();
     JNI_CALL(jobject, java_symbol, graal_isolate, GraalAccessMethod::symbol_private_for_api, Object, java_description);
-    GraalSymbol* graal_symbol = new GraalSymbol(graal_isolate, java_symbol);
+    GraalSymbol* graal_symbol = GraalSymbol::Allocate(graal_isolate, java_symbol);
     return reinterpret_cast<v8::Private*> (graal_symbol);
 }
 
