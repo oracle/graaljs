1376ad34be3ced76c141f78c85ac932bcc08e28c
Andreas Woess <andreas.woess@oracle.com>
2023-05-22 15:30:44 +0200

    Add basic AsyncContext test.

---
+++ graal-js/src/com.oracle.truffle.js.test/js/async-context-test.js
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2023, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * Licensed under the Universal Permissive License v 1.0 as shown at http://oss.oracle.com/licenses/upl.
+ */
+
+/**
+ * Tests AsyncContext proposal.
+ *
+ * @option unhandled-rejections=throw
+ * @option async-context
+ * @option testV8-mode
+ */
+
+load("assert.js");
+
+const context = new AsyncContext({name: 'ctx'});
+const timeout = 10; // does not really matter
+
+if (typeof setTimeout === 'undefined' && typeof TestV8 !== 'undefined') {
+  setTimeout = TestV8.setTimeout;
+}
+
+context.run("top", main);
+
+function main() {
+  setTimeout(() => {
+    assertSame(context.get(), 'top');
+
+    context.run("A", () => {
+      assertSame(context.get(), 'A');
+
+      setTimeout(() => {
+        assertSame(context.get(), 'A');
+      }, timeout);
+    });
+  }, timeout);
+
+  context.run("B", () => {
+    assertSame(context.get(), 'B');
+
+    setTimeout(() => {
+      assertSame(context.get(), 'B');
+    }, timeout);
+  });
+
+  assertSame(context.get(), 'top');
+
+  const snapshotDuringTop = AsyncContext.wrap((cb) => {
+    assertSame(context.get(), 'top');
+    cb();
+  });
+
+  context.run("C", () => {
+    assertSame(context.get(), 'C');
+
+    snapshotDuringTop(() => {
+      assertSame(context.get(), 'top');
+    });
+  });
+}
