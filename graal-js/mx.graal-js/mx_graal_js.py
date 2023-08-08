76fabc0679660008b5a7b1acbab5088e875e79e9
Andreas Woess <andreas.woess@oracle.com>
2023-08-08 15:56:31 +0200

    Remove Truffle macro dependency again.

--- graal-js/mx.graal-js/mx_graal_js.py
+++ graal-js/mx.graal-js/mx_graal_js.py
@@ -400,9 +400,7 @@ mx_sdk.register_graalvm_component(mx_sdk.GraalVmLanguage(
         'Truffle',
         'TRegex',
         'ICU4J',
-    ] + [
-        'tflm', # Truffle Macro
-    ] if mx.suite('substratevm', fatalIfMissing=False) else [],
+    ],
     truffle_jars=[
         'graal-js:GRAALJS',
     ],
