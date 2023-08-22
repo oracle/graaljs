b9e226343b2a81158ddb464efb27b21c02ea2d9a
Andreas Woess <andreas.woess@oracle.com>
2023-08-22 19:32:02 +0200

    Add `mx node --standalone[=jvm|native]` option.

--- graal-nodejs/mx.graal-nodejs/mx_graal_nodejs.py
+++ graal-nodejs/mx.graal-nodejs/mx_graal_nodejs.py
@@ -371,7 +371,7 @@ def testnodeInstrument(args, nonZeroIsFatal=True, out=None, err=None, cwd=None):
     node(['--experimental-options', '--coverage-like-instrument', '-e', '6*7'], nonZeroIsFatal=nonZeroIsFatal, out=out, err=err, cwd=cwd)
 
 def testnode(args, nonZeroIsFatal=True, out=None, err=None, cwd=None):
-    mode, vmArgs, progArgs = setupNodeEnvironment(args)
+    mode, vmArgs, progArgs, _ = setupNodeEnvironment(args)
     if mode == 'Debug':
         progArgs += ['-m', 'debug']
     extraArgs = ['-Xmx8g'] if not any(vmArg.startswith('-Xmx') for vmArg in vmArgs) else []
@@ -408,15 +408,25 @@ def processDevkitRoot(env=None):
 
 def setupNodeEnvironment(args, add_graal_vm_args=True):
     args = args if args else []
-    mode, vmArgs, progArgs = _parseArgs(args)
+    mode, vmArgs, progArgs, standalone = _parseArgs(args)
     setLibraryPath()
 
     if _is_windows:
         processDevkitRoot()
 
+    nodeExe = join(_suite.dir, 'out', mode, 'node')
+    if standalone:
+        if mx.suite('vm', fatalIfMissing=False) is not None:
+            import mx_sdk_vm_impl
+            standalone_home = mx_sdk_vm_impl.standalone_home('nodejs', is_jvm=standalone == 'jvm')
+            nodeExe = join(standalone_home, 'bin', 'node')
+            return mode, vmArgs, progArgs, nodeExe
+        else:
+            mx.warn("No standalones available. Falling back to default mx node command.\nYou need to dynamically import '/vm,/substratevm' (or at least '/vm'). Example: 'mx --env svm node'")
+
     if mx.suite('vm', fatalIfMissing=False) is not None and mx.suite('substratevm', fatalIfMissing=False) is not None:
         _prepare_svm_env()
-        return mode, vmArgs, progArgs
+        return mode, vmArgs, progArgs, nodeExe
 
     if mx.suite('vm', fatalIfMissing=False) is not None or mx.suite('substratevm', fatalIfMissing=False) is not None:
         mx.warn("Running on the JVM.\nIf you want to run on SubstrateVM, you need to dynamically import both '/substratevm' and '/vm'.\nExample: 'mx --env svm node'")
@@ -434,17 +444,17 @@ def setupNodeEnvironment(args, add_graal_vm_args=True):
     prevPATH = os.environ['PATH']
     _setEnvVar('PATH', "%s%s%s" % (join(_suite.mxDir, 'fake_launchers'), pathsep, prevPATH))
 
-    return mode, vmArgs, progArgs
+    return mode, vmArgs, progArgs, nodeExe
 
 def makeInNodeEnvironment(args):
-    argGroups = setupNodeEnvironment(args)
-    _setEnvVar('NODE_JVM_OPTIONS', ' '.join(argGroups[1]))
+    _, vmArgs, progArgs, _ = setupNodeEnvironment(args)
+    _setEnvVar('NODE_JVM_OPTIONS', ' '.join(vmArgs))
     if _is_windows:
         _mxrun([join('.', 'vcbuild.bat'),
                 'noprojgen',
                 'nobuild',
                 'java-home', _java_home()
-            ] + argGroups[2], cwd=_suite.dir)
+            ] + progArgs, cwd=_suite.dir)
     else:
         makeCmd = mx.gmake_cmd()
         if _current_os == 'solaris':
@@ -454,7 +464,7 @@ def makeInNodeEnvironment(args):
             _mxrun(['sh', '-c', 'ln -s `which gcp` ' + join(_suite.dir, 'cp')])
             prevPATH = os.environ['PATH']
             _setEnvVar('PATH', "%s:%s" % (_suite.dir, prevPATH))
-        _mxrun([makeCmd] + argGroups[2], cwd=_suite.dir)
+        _mxrun([makeCmd] + progArgs, cwd=_suite.dir)
         if _current_os == 'solaris':
             _mxrun(['rm', 'cp'])
 
@@ -462,7 +472,7 @@ def prepareNodeCmdLine(args, add_graal_vm_args=True):
     '''run a Node.js program or shell
         --debug to run in debug mode (provided that you build it)
     '''
-    mode, vmArgs, progArgs = setupNodeEnvironment(args, add_graal_vm_args)
+    _, vmArgs, progArgs, nodeExe = setupNodeEnvironment(args, add_graal_vm_args)
 
     if mx_gate.get_jacoco_agent_args():
         # The node launcher (i.e., JNI) does not support @argfile vm args, so we have to expand them to ordinary args
@@ -484,7 +494,7 @@ def prepareNodeCmdLine(args, add_graal_vm_args=True):
     vmArgs += [parentVmArg for parentVmArg in os.environ.get('NODE_JVM_OPTIONS', '').split() if not parentVmArg in vmArgs]
 
     _setEnvVar('NODE_JVM_OPTIONS', ' '.join(vmArgs))
-    return [join(_suite.dir, 'out', mode, 'node')] + progArgs
+    return [nodeExe] + progArgs
 
 def parse_js_args(args):
     vmArgs, progArgs = mx_graal_js.parse_js_args(args)
@@ -513,11 +523,16 @@ def _has_jvmci(forBuild=False):
     return get_jdk(forBuild=forBuild).tag == 'jvmci'
 
 def _parseArgs(args):
-    arguments = list(args)
-    debugArg = '--debug'
-    if debugArg in arguments:
+    parser = ArgumentParser(prog='mx node', allow_abbrev=False, add_help=False)
+    parser.add_argument('--debug', action='store_true', help='Run in debug mode')
+    parser.add_argument('--standalone', action='store_const', const='native', help='Run standalone')
+    parser.add_argument('--standalone=native', dest='standalone', action='store_const', const='native', help='Run native standalone')
+    parser.add_argument('--standalone=jvm', dest='standalone', action='store_const', const='jvm', help='Run jvm standalone')
+    opts, arguments = parser.parse_known_args(args)
+
+    standalone = opts.standalone
+    if opts.debug:
         mx.log('Running in debug mode. The --debug argument is handled by mx and not passed as program argument')
-        arguments.remove(debugArg)
         mode = 'Debug'
     else:
         mode = 'Release'
@@ -537,8 +552,9 @@ def _parseArgs(args):
     mx.logv('[_parseArgs] mode: %s' % mode)
     mx.logv('[_parseArgs] vmArgs: %s' % vmArgs)
     mx.logv('[_parseArgs] progArgs: %s' % progArgs)
+    mx.logv('[_parseArgs] standalone: %s' % standalone)
 
-    return mode, vmArgs, progArgs
+    return mode, vmArgs, progArgs, standalone
 
 def overrideBuild():
     def build(args):
