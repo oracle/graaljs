45e09c7852f8253878dfd490e0e3ca5d01dd6d7b
Andreas Woess <andreas.woess@oracle.com>
2024-01-05 16:02:59 +0100

    Property assumptions on null shape are trivially always true.

--- graal-js/src/com.oracle.truffle.js/src/com/oracle/truffle/js/runtime/objects/JSShape.java
+++ graal-js/src/com.oracle.truffle.js/src/com/oracle/truffle/js/runtime/objects/JSShape.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, 2023, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2018, 2024, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * The Universal Permissive License (UPL), Version 1.0
@@ -178,7 +178,10 @@ public final class JSShape {
     }
 
     public static Assumption getPropertyAssumption(Shape shape, Object key, boolean prototype) {
-        assert JSRuntime.isPropertyKey(key) || key instanceof HiddenKey;
+        assert JSRuntime.isPropertyKey(key) || key instanceof HiddenKey : key;
+        if (shape == Null.SHAPE) {
+            return Assumption.ALWAYS_VALID;
+        }
         if (prototype && JSConfig.LeafShapeAssumption) {
             return shape.getLeafAssumption();
         }
