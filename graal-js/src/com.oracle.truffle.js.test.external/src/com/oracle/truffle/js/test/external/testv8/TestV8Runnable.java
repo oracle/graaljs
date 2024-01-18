d49325488d58b965f9f041a77c0636e5a0bd008b
Andreas Woess <andreas.woess@oracle.com>
2024-01-18 16:15:43 +0100

    Enable Array.fromAsync feature.

--- graal-js/src/com.oracle.truffle.js.test.external/src/com/oracle/truffle/js/test/external/testv8/TestV8Runnable.java
+++ graal-js/src/com.oracle.truffle.js.test.external/src/com/oracle/truffle/js/test/external/testv8/TestV8Runnable.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, 2023, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2018, 2024, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * The Universal Permissive License (UPL), Version 1.0
@@ -110,6 +110,7 @@ public class TestV8Runnable extends TestRunnable {
     });
     private static final Set<String> STAGING_FLAGS = featureSet(new String[]{
                     "--harmony",
+                    "--harmony-array-from-async",
                     "--harmony-array-grouping",
                     "--harmony-atomics-waitasync",
                     "--harmony-intl-locale-info-func",
