3c1eed7061ece524f4cf94ef0ca45546170a75ef
Andreas Woess <andreas.woess@oracle.com>
2024-02-17 07:41:33 +0100

    Remove needless serialVersionUID.

--- graal-js/src/com.oracle.truffle.js/src/com/oracle/truffle/js/runtime/BigInt.java
+++ graal-js/src/com.oracle.truffle.js/src/com/oracle/truffle/js/runtime/BigInt.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, 2023, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2018, 2024, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * The Universal Permissive License (UPL), Version 1.0
@@ -58,8 +58,6 @@ import com.oracle.truffle.js.runtime.interop.JSMetaType;
 @ExportLibrary(InteropLibrary.class)
 @ValueType
 public final class BigInt implements Comparable<BigInt>, TruffleObject {
-
-    static final long serialVersionUID = 6019523258212492110L;
 
     private final BigInteger value;
     private final boolean foreign;
