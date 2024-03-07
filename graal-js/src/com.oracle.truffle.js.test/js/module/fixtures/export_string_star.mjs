8a84c330f5713f3e9fe677e18c046ce953d09a4c
Jan Stola <jan.stola@oracle.com>
2024-03-07 18:48:29 +0100

    Testing a potential clash between "*" import and a star import.

---
+++ graal-js/src/com.oracle.truffle.js.test/js/module/fixtures/export_string_star.mjs
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2024, 2024, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * Licensed under the Universal Permissive License v 1.0 as shown at http://oss.oracle.com/licenses/upl.
+ */
+
+const foo = 42;
+export {
+    foo as "*",
+    foo as star,
+};
---
+++ graal-js/src/com.oracle.truffle.js.test/js/module/import_string_star.mjs
@@ -0,0 +1,9 @@
+/*
+ * Copyright (c) 2024, 2024, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * Licensed under the Universal Permissive License v 1.0 as shown at http://oss.oracle.com/licenses/upl.
+ */
+
+import { star, "*" as bar } from "./fixtures/export_string_star.mjs";
+if (star !== bar) throw new Error();
