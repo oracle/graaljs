a8406db8cf510a7b8826f2741827e8b33c03cc72
Andreas Woess <andreas.woess@oracle.com>
2024-03-07 15:41:04 +0100

    [GR-52569] Fix isResultArrayIndex check for ToArrayIndexNode without ToPropertyKey conversion.

--- graal-js/src/com.oracle.truffle.js/src/com/oracle/truffle/js/nodes/cast/ToArrayIndexNode.java
+++ graal-js/src/com.oracle.truffle.js/src/com/oracle/truffle/js/nodes/cast/ToArrayIndexNode.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, 2023, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2018, 2024, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * The Universal Permissive License (UPL), Version 1.0
@@ -76,7 +76,7 @@ public abstract class ToArrayIndexNode extends JavaScriptBaseNode {
 
     @SuppressWarnings("static-method")
     public final boolean isResultArrayIndex(Object result) {
-        return result instanceof Long;
+        return result instanceof Long index && JSRuntime.isArrayIndex(index);
     }
 
     protected ToArrayIndexNode(boolean convertToPropertyKey, boolean convertStringToIndex) {
