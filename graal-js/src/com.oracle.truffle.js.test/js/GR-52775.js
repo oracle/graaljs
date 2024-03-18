15f66969d79fcda58aeb9d648d142a7d4907e5fb
Jan Stola <jan.stola@oracle.com>
2024-03-18 13:12:44 +0100

    Testing Array.prototype.sort when arrayPrototypeNoElementsAssumption is not valid.

--- graal-js/src/com.oracle.truffle.js.test/js/GR-52775.js
+++ graal-js/src/com.oracle.truffle.js.test/js/GR-52775.js
@@ -30,3 +30,14 @@ assertThrows(() => array.sort(), TypeError); // non-writable property
 array = [Math.PI];
 Object.defineProperty(array, '1', { writable: true, value: '1foo' });
 assertSameContent(['1foo', Math.PI], array.sort());
+
+// invalidate arrayPrototypeNoElementsAssumption
+Object.defineProperty(Array.prototype, '1', { writable: true, value: 'foo' });
+
+array = [2,3,4];
+delete array[1];
+assertSameContent([2, 4, 'foo'], array.sort());
+
+array = [2.3,3.4,4.5];
+delete array[1];
+assertSameContent([2.3, 4.5, 'foo'], array.sort());
