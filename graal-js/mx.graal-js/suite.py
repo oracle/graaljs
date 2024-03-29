878a20d801d12b47833e3d61a3b2812e51fd0e37
ol-automation_ww <ol-automation_ww@oracle.com>
2024-03-29 21:04:09 +0000

    Update graal import.

--- graal-js/mx.graal-js/suite.py
+++ graal-js/mx.graal-js/suite.py
@@ -24,7 +24,7 @@ suite = {
         {
            "name" : "regex",
            "subdir" : True,
-           "version" : "4b1e416ef35463af839c2cd3757d92fba9929b85",
+           "version" : "88992ba574223484445ce4b985a359be30a386a4",
            "urls" : [
                 {"url" : "https://github.com/oracle/graal.git", "kind" : "git"},
             ]
