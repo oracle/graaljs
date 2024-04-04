f624fda286cc6149ec4ffdd6684985e9e608ef3b
Jan Stola <jan.stola@oracle.com>
2024-04-04 22:16:48 +0200

    Accepting --harmony-import-attributes option.

--- graal-nodejs/mx.graal-nodejs/com.oracle.truffle.trufflenode/src/com/oracle/truffle/trufflenode/Options.java
+++ graal-nodejs/mx.graal-nodejs/com.oracle.truffle.trufflenode/src/com/oracle/truffle/trufflenode/Options.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, 2023, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2018, 2024, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * The Universal Permissive License (UPL), Version 1.0
@@ -232,7 +232,7 @@ public final class Options {
                     polyglotOptions.put("js.top-level-await", "true");
                     continue;
                 }
-                if ("harmony-import-assertions".equals(normalizedKey)) {
+                if ("harmony-import-assertions".equals(normalizedKey) || "harmony-import-attributes".equals(normalizedKey)) {
                     polyglotOptions.put("js.import-assertions", "true");
                     continue;
                 }
