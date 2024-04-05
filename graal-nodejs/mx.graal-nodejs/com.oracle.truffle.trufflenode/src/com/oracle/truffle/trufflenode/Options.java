9a6f7a14185c149db8d34aabd7900682dd15b79e
Jan Stola <jan.stola@oracle.com>
2024-04-05 23:50:41 +0200

    Do not refuse (accept but ignore) --trace-gc option.

--- graal-nodejs/mx.graal-nodejs/com.oracle.truffle.trufflenode/src/com/oracle/truffle/trufflenode/Options.java
+++ graal-nodejs/mx.graal-nodejs/com.oracle.truffle.trufflenode/src/com/oracle/truffle/trufflenode/Options.java
@@ -157,6 +157,7 @@ public final class Options {
                         "nolazy",
                         "nouse-idle-notification",
                         "stack-size",
+                        "trace-gc",
                         "use-idle-notification"
         }));
 
