5b9e608a800e418c2d995c19912a866b8ea05d71
Jan Stola <jan.stola@oracle.com>
2024-04-28 19:43:58 +0200

    Correction of the handling of long in TemporalUtil.getOffsetNanosecondsFor().

--- graal-js/src/com.oracle.truffle.js.test/js/GR-53714.js
+++ graal-js/src/com.oracle.truffle.js.test/js/GR-53714.js
@@ -7,6 +7,8 @@
 
 /**
  * Regression test of the handling of long values.
+ * 
+ * @option temporal
  */
 
 load('assert.js');
@@ -27,3 +29,7 @@ assertSame(JSON.stringify(o, null, 20), JSON.stringify(o, null, long20));
 assertSame(JSON.stringify(o, [42]), JSON.stringify(o, [long42]));
 
 assertThrows(() => (0 instanceof long0), TypeError);
+
+var fooTimeZone = { getOffsetNanosecondsFor() { return long42; }, getPossibleInstantsFor() {}, id: 'foo' };
+var zonedDateTime = new Temporal.ZonedDateTime(0n, fooTimeZone);
+assertSame(42, zonedDateTime.offsetNanoseconds);
