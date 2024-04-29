bbeafba78ddc6195d207f34dc20d67e766a9a3e6
Andreas Woess <andreas.woess@oracle.com>
2024-04-29 14:01:17 +0200

    Simplify unbound function check in ExportValueNode.

--- graal-js/src/com.oracle.truffle.js/src/com/oracle/truffle/js/nodes/interop/ExportValueNode.java
+++ graal-js/src/com.oracle.truffle.js/src/com/oracle/truffle/js/nodes/interop/ExportValueNode.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, 2023, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2018, 2024, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * The Universal Permissive License (UPL), Version 1.0
@@ -95,13 +95,13 @@ public abstract class ExportValueNode extends JavaScriptBaseNode {
         return function;
     }
 
-    @Specialization(guards = {"bindFunctions", "!isUndefined(thiz)", "!isBoundJSFunction(function)", "!isInteropCompletePromises() || !isAsyncFunction(function)"})
-    protected static TruffleObject doBindUnboundFunction(JSFunctionObject function, Object thiz, @SuppressWarnings("unused") boolean bindFunctions) {
+    @Specialization(guards = {"bindFunctions", "!isUndefined(thiz)", "!isInteropCompletePromises() || !isAsyncFunction(function)"})
+    protected static TruffleObject doBindUnboundFunction(JSFunctionObject.Unbound function, Object thiz, @SuppressWarnings("unused") boolean bindFunctions) {
         return new InteropBoundFunction(function, thiz);
     }
 
-    @Specialization(guards = {"bindFunctions", "isBoundJSFunction(function)", "!isInteropCompletePromises() || !isAsyncFunction(function)"})
-    protected static JSDynamicObject doBoundFunction(JSFunctionObject function, @SuppressWarnings("unused") Object thiz, @SuppressWarnings("unused") boolean bindFunctions) {
+    @Specialization(guards = {"bindFunctions", "!isInteropCompletePromises() || !isAsyncFunction(function)"})
+    protected static JSDynamicObject doBoundFunction(JSFunctionObject.BoundOrWrapped function, @SuppressWarnings("unused") Object thiz, @SuppressWarnings("unused") boolean bindFunctions) {
         return function;
     }
 
