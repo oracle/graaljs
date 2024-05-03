2183191b4153914b45eb4c1cbc9a17fc1b5e669d
Andreas Woess <andreas.woess@oracle.com>
2024-05-03 10:06:17 +0200

    Handle SafeInteger in JSRuntime.doubleValue directly.
    (cherry picked from commit 60f197696f93122968438c72abb6f5f293a52a6b)

--- graal-js/src/com.oracle.truffle.js/src/com/oracle/truffle/js/runtime/JSRuntime.java
+++ graal-js/src/com.oracle.truffle.js/src/com/oracle/truffle/js/runtime/JSRuntime.java
@@ -1970,6 +1970,9 @@ public final class JSRuntime {
         if (number instanceof Integer) {
             return ((Integer) number).doubleValue();
         }
+        if (number instanceof SafeInteger) {
+            return ((SafeInteger) number).doubleValue();
+        }
         return doubleValueVirtual(number);
     }
 
