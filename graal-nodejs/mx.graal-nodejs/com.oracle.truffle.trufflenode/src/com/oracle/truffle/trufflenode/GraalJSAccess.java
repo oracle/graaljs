d3952c6edc158c6dd117a7a2b13f001275fbb185
Jan Stola <jan.stola@oracle.com>
2024-07-14 18:39:35 +0200

    Do not attempt to get a namespace object for an unlinked module.

--- graal-nodejs/mx.graal-nodejs/com.oracle.truffle.trufflenode/src/com/oracle/truffle/trufflenode/GraalJSAccess.java
+++ graal-nodejs/mx.graal-nodejs/com.oracle.truffle.trufflenode/src/com/oracle/truffle/trufflenode/GraalJSAccess.java
@@ -2718,6 +2718,11 @@ public final class GraalJSAccess {
                 target = jsObject;
                 key = HIDDEN_WEAK_CALLBACK;
             } else if (object instanceof JSModuleRecord moduleRecord) {
+                if (moduleRecord.getStatus() == JSModuleRecord.Status.Unlinked) {
+                    assert (callbackPointer == 0);
+                    // ClearWeak() called on a module that cannot be weak yet
+                    return null;
+                }
                 target = moduleRecord.getContext().getEvaluator().getModuleNamespace(moduleRecord);
                 key = HIDDEN_WEAK_CALLBACK_SUBSTITUTE;
             } else {
