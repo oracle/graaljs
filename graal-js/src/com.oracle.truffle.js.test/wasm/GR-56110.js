97bd6d468fb2a41ceee3ac8ec432c5ff40290c75
Andreas Woess <andreas.woess@oracle.com>
2024-07-24 15:54:09 +0200

    Improve error message for when imported module is not an object.
    (cherry picked from commit 91829eeb62a25b08ce2e7702292268c3dc1d3639)

---
+++ graal-js/src/com.oracle.truffle.js.test/wasm/GR-56110.js
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2024, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * Licensed under the Universal Permissive License v 1.0 as shown at http://oss.oracle.com/licenses/upl.
+ */
+
+/**
+ * Test error messages for missing importObject keys or illegal values.
+ *
+ * @option webassembly
+ */
+
+load('../js/assert.js');
+
+//(module
+//   (type (func (result i32)))
+//   (import "m" "f" (func (type 0)))
+//)
+const bytes = new Uint8Array([
+    0x0,0x61,0x73,0x6d,0x1,0x0,0x0,0x0,
+    0x1,0x5,0x1,0x60,0x0,0x1,0x7f,
+    0x2,0x7,0x1,0x1,0x6d,0x1,0x66,0x0,0x0]);
+
+const wasmModule = new WebAssembly.Module(bytes);
+assertThrows(() => new WebAssembly.Instance(wasmModule, {}), TypeError, 'Imported module "m" is not an object');
