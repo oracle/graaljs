6807c79fa615998c1a0589e3c5bd0c71129fc652
Jan Stola <jan.stola@oracle.com>
2024-08-08 16:59:12 +0200

    Testing the growing of an array (using splice) from the beginning.

---
+++ graal-js/src/com.oracle.truffle.js.test/js/GR-57179.js
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2024, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * Licensed under the Universal Permissive License v 1.0 as shown at http://oss.oracle.com/licenses/upl.
+ */
+
+load("assert.js");
+
+var array = [,-1];
+for (var i = 1; i <= 1000; i++) {
+    array.splice(0,0,i);
+}
+
+assertSame(1002, array.length);
+for (var i = 0; i < array.length; i++) {
+    var expected = (i === 1000) ? undefined : (1000 - i);
+    assertSame(expected, array[i]);
+}
