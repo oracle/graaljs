11b435ca11510332735471c8b8676209b7d11dd6
Jan Stola <jan.stola@oracle.com>
2024-09-14 09:07:48 +0200

    Broadcast should create a new instance of JSArrayBufferObject.Shared for each agent.

--- graal-js/src/com.oracle.truffle.js/src/com/oracle/truffle/js/runtime/util/DebugJSAgent.java
+++ graal-js/src/com.oracle.truffle.js/src/com/oracle/truffle/js/runtime/util/DebugJSAgent.java
@@ -66,6 +66,7 @@ import com.oracle.truffle.js.runtime.JSRealm;
 import com.oracle.truffle.js.runtime.builtins.JSArrayBufferObject;
 import com.oracle.truffle.js.runtime.builtins.JSFunction;
 import com.oracle.truffle.js.runtime.builtins.JSFunctionObject;
+import com.oracle.truffle.js.runtime.builtins.JSSharedArrayBuffer;
 import com.oracle.truffle.js.runtime.objects.Null;
 
 /**
@@ -134,9 +135,15 @@ public class DebugJSAgent extends JSAgent {
                         }
                         // Signal received or timeout. Process all pending events.
                         do {
-                            JSArrayBufferObject.Shared next = executor.broadcasts.poll();
-                            if (next != null) {
-                                executor.executeBroadcastCallback(next);
+                            JSArrayBufferObject.Shared original = executor.broadcasts.poll();
+                            if (original != null) {
+                                // Create SharedArrayBuffer for this agent
+                                // (sharing the ByteBuffer with the original)
+                                JSArrayBufferObject.Shared current = (JSArrayBufferObject.Shared) JSSharedArrayBuffer.createSharedArrayBuffer(innerContext.getContext(), innerContext,
+                                                original.getByteBuffer());
+                                current.setWaiterList(original.getWaiterList());
+
+                                executor.executeBroadcastCallback(current);
                                 // broadcast callback may have called agent.leaving().
                                 if (childAgent.quit) {
                                     return;
