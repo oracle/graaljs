0fa0d0551fe6b3e9da5764e67d7672f5f5fc3d06
Andreas Woess <andreas.woess@oracle.com>
2024-10-24 01:42:02 +0200

    Fix typo.
    (cherry picked from commit 919b09f7cf597bbf92abdebd1bde7aedf66f70a5)

--- graal-js/src/com.oracle.truffle.js/src/com/oracle/truffle/js/runtime/util/UTS35Validator.java
+++ graal-js/src/com.oracle.truffle.js/src/com/oracle/truffle/js/runtime/util/UTS35Validator.java
@@ -177,7 +177,7 @@ public class UTS35Validator {
 
     private static String extensions() {
         // extensions = unicode_locale_extensions | transformed_extensions | other_extensions
-        return group(unicodeLocaleExtensions() + "|" + transformedExtentensions() + "|" + otherExtensions());
+        return group(unicodeLocaleExtensions() + "|" + transformedExtensions() + "|" + otherExtensions());
     }
 
     private static String unicodeLocaleExtensions() {
@@ -185,7 +185,7 @@ public class UTS35Validator {
         return group(sep() + "[uU]" + group(group(sep() + keyword()) + "+|" + group(sep() + attribute()) + "+" + group(sep() + keyword()) + "*"));
     }
 
-    private static String transformedExtentensions() {
+    private static String transformedExtensions() {
         // transformed_extensions = sep [tT] ((sep tlang (sep tfield)*) | (sep tfield)+)
         return group(sep() + "[tT]" + group(group(sep() + tLang() + group(sep() + tField()) + "*") + "|" + group(sep() + tField()) + "+"));
     }
