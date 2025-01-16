377b5f76cb7f7f57bb3e441fc9e27b92fbb9cc91
Andreas Woess <andreas.woess@oracle.com>
2025-01-16 16:02:31 +0100

    Implement Iterator.concat() a.k.a. iterator-sequencing proposal.

--- graal-js/src/com.oracle.truffle.js/src/com/oracle/truffle/js/runtime/JSContext.java
+++ graal-js/src/com.oracle.truffle.js/src/com/oracle/truffle/js/runtime/JSContext.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, 2024, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2018, 2025, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * The Universal Permissive License (UPL), Version 1.0
@@ -354,6 +354,7 @@ public class JSContext {
         AsyncIteratorFindWithResult,
         AsyncGeneratorAwaitReturnFulfilled,
         AsyncGeneratorAwaitReturnRejected,
+        IteratorConcat,
         IteratorMap,
         IteratorFilter,
         IteratorTake,
