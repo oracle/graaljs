4d2b5a919593b49f54546e2a4a8f2fd11d3501da
Andreas Woess <andreas.woess@oracle.com>
2025-02-03 13:34:16 +0100

    Add support for skipping tests based on JDK version.

--- graal-js/src/com.oracle.truffle.js.test.external/src/com/oracle/truffle/js/test/external/suite/TestFileUtil.java
+++ graal-js/src/com.oracle.truffle.js.test.external/src/com/oracle/truffle/js/test/external/suite/TestFileUtil.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, 2020, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2018, 2025, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * The Universal Permissive License (UPL), Version 1.0
@@ -77,6 +77,8 @@ public final class TestFileUtil {
         mergedTestFile.setBlockedBy(blockedBy != null ? blockedBy : first.getBlockedBy());
         String comment = second.getComment();
         mergedTestFile.setComment(comment != null ? comment : first.getComment());
+        TestFile.JavaVersion javaVersion = second.getJavaVersion();
+        mergedTestFile.setJavaVersion(javaVersion != null ? javaVersion : first.getJavaVersion());
         return mergedTestFile;
     }
 
