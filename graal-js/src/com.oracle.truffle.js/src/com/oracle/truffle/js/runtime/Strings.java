48f4bc07c1f1b338905192682143bb0d160b3399
Jan Stola <jan.stola@oracle.com>
2025-02-12 21:23:58 +0100

    Using an explicit locale Strings.format().

--- graal-js/src/com.oracle.truffle.js/src/com/oracle/truffle/js/runtime/Strings.java
+++ graal-js/src/com.oracle.truffle.js/src/com/oracle/truffle/js/runtime/Strings.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021, 2024, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2021, 2025, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * The Universal Permissive License (UPL), Version 1.0
@@ -1031,7 +1031,7 @@ public final class Strings {
     }
 
     public static TruffleString format(String formatString, Object... args) {
-        return fromJavaString(String.format(formatString, args));
+        return fromJavaString(String.format(Locale.ROOT, formatString, args));
     }
 
     @TruffleBoundary
