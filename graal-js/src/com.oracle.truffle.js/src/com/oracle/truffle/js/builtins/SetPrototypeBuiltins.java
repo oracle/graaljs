94b2e03f3b9303ce2c6008eafe59399f1a1893ea
Jan Stola <jan.stola@oracle.com>
2025-02-20 10:24:23 +0100

    Set.prototype.difference should iterate over the copied [[SetData]].

--- graal-js/src/com.oracle.truffle.js/src/com/oracle/truffle/js/builtins/SetPrototypeBuiltins.java
+++ graal-js/src/com.oracle.truffle.js/src/com/oracle/truffle/js/builtins/SetPrototypeBuiltins.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, 2024, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2018, 2025, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * The Universal Permissive License (UPL), Version 1.0
@@ -448,7 +448,7 @@ public final class SetPrototypeBuiltins extends JSBuiltinsContainer.SwitchEnum<S
             int thisSize = thisSetData.size();
             if (thisSize <= otherRec.size()) {
                 thisSetSmallerProfile.enter(this);
-                JSHashMap.Cursor cursor = thisSetData.getEntries();
+                JSHashMap.Cursor cursor = resultSetData.getEntries();
                 while (cursor.advance()) {
                     Object e = cursor.getKey();
                     Object inOtherObj = call(otherRec.has(), otherRec.set(), e);
