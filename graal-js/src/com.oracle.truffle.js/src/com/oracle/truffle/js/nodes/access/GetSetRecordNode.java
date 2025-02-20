e65acbdb4cf6f0a903cc1c39defcfea4a8e345c1
Jan Stola <jan.stola@oracle.com>
2025-02-20 10:25:57 +0100

    Updating an error message in GetSetRecordNode.

--- graal-js/src/com.oracle.truffle.js/src/com/oracle/truffle/js/nodes/access/GetSetRecordNode.java
+++ graal-js/src/com.oracle.truffle.js/src/com/oracle/truffle/js/nodes/access/GetSetRecordNode.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023, 2023, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2023, 2025, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * The Universal Permissive License (UPL), Version 1.0
@@ -101,7 +101,7 @@ public abstract class GetSetRecordNode extends JavaScriptBaseNode {
         double doubleSize = numberToDoubleValueNode.execute(this, intSize);
         if (doubleSize < 0) {
             errorBranch.enter(this);
-            throw Errors.createRangeError("The .size property is negative", this);
+            throw Errors.createRangeErrorFormat("'%s' is an invalid size", this, JSRuntime.safeToString(intSize));
         }
 
         Object has = getHasNode.getValue(obj);
