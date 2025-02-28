5daa6f0e4879d63a0b8d5cb599ac834c510cb436
Jan Stola <jan.stola@oracle.com>
2025-02-28 09:55:32 +0100

    Correction of the message of an error from JSProxyCallNode.doConstruct().

--- graal-js/src/com.oracle.truffle.js/src/com/oracle/truffle/js/nodes/access/JSProxyCallNode.java
+++ graal-js/src/com.oracle.truffle.js/src/com/oracle/truffle/js/nodes/access/JSProxyCallNode.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, 2024, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2018, 2025, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * The Universal Permissive License (UPL), Version 1.0
@@ -133,7 +133,7 @@ public abstract class JSProxyCallNode extends JavaScriptBaseNode {
 
         if (!isConstructor.executeBoolean(JSProxy.getTarget(proxy))) {
             errorBranch.enter(this);
-            throw Errors.createTypeErrorNotAFunction(function, this);
+            throw Errors.createTypeErrorNotAConstructor(function, this, getJSContext());
         } else {
             JSDynamicObject pxHandler = JSProxy.getHandler(proxy);
             if (pxHandler == Null.instance) {
