6f40baf18789cd59a5c3e71d96614193e82e3efb
Jan Stola <jan.stola@oracle.com>
2025-03-07 22:49:31 +0100

    Constructor of a promise can be a foreign instantiable.

--- graal-js/src/com.oracle.truffle.js/src/com/oracle/truffle/js/builtins/PromisePrototypeBuiltins.java
+++ graal-js/src/com.oracle.truffle.js/src/com/oracle/truffle/js/builtins/PromisePrototypeBuiltins.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, 2023, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2018, 2025, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * The Universal Permissive License (UPL), Version 1.0
@@ -251,7 +251,7 @@ public final class PromisePrototypeBuiltins extends JSBuiltinsContainer.SwitchEn
                     Object onFinally = getOnFinally.getValue(functionObject);
                     assert JSRuntime.isCallable(onFinally);
                     Object result = callFinally.executeCall(JSArguments.createZeroArg(Undefined.instance, onFinally));
-                    JSDynamicObject constructor = (JSDynamicObject) getConstructor.getValue(functionObject);
+                    Object constructor = getConstructor.getValue(functionObject);
                     assert JSRuntime.isConstructor(constructor);
                     JSDynamicObject promise = promiseResolve.execute(constructor, result);
                     Object value = valueNode.execute(frame);
