bdda10b985d23a2ee71b9d72a4da0012eb504a77
Jan Stola <jan.stola@oracle.com>
2025-03-25 19:52:21 +0100

    Correction of the typo in JSNumberFormat.formatRangeToParts.

--- graal-js/src/com.oracle.truffle.js/src/com/oracle/truffle/js/runtime/builtins/intl/JSNumberFormat.java
+++ graal-js/src/com.oracle.truffle.js/src/com/oracle/truffle/js/runtime/builtins/intl/JSNumberFormat.java
@@ -572,7 +572,7 @@ public final class JSNumberFormat extends JSNonProxy implements JSConstructorFac
                 if (field == NumberFormat.Field.SIGN) {
                     type = isPlusSign(value) ? IntlUtil.PLUS_SIGN : IntlUtil.MINUS_SIGN;
                 } else if (field == NumberFormat.Field.INTEGER) {
-                    Object val = IntlUtil.END_RANGE.equals(source) ? y : y;
+                    Number val = IntlUtil.END_RANGE.equals(source) ? y : x;
                     assert !JSRuntime.isNaN(val);
                     if (val instanceof Double doubleValue && Double.isInfinite(doubleValue)) {
                         type = IntlUtil.INFINITY;
