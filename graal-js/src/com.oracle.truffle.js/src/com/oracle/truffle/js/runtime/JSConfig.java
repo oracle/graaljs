822c2b09cff77e9e6940f134d3286c87f73cce88
Jan Stola <jan.stola@oracle.com>
2025-05-07 14:42:52 +0200

    Adding ECMAScript2026 and making ECMAScript2025 the default version.

--- graal-js/src/com.oracle.truffle.js/src/com/oracle/truffle/js/runtime/JSConfig.java
+++ graal-js/src/com.oracle.truffle.js/src/com/oracle/truffle/js/runtime/JSConfig.java
@@ -79,9 +79,10 @@ public final class JSConfig {
     public static final int ECMAScript2023 = 14;
     public static final int ECMAScript2024 = 15;
     public static final int ECMAScript2025 = 16;
+    public static final int ECMAScript2026 = 17;
     public static final int ECMAScriptVersionYearDelta = 2009; // ES6==ES2015
-    public static final int LatestECMAScriptVersion = ECMAScript2024;
-    public static final int StagingECMAScriptVersion = ECMAScript2025;
+    public static final int LatestECMAScriptVersion = ECMAScript2025;
+    public static final int StagingECMAScriptVersion = ECMAScript2026;
     /** Enable Annex B "Additional ECMAScript Features for Web Browsers". */
     public static final boolean AnnexB = true;
 
