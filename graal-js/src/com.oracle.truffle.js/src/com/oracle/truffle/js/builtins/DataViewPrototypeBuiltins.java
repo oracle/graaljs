8bdc9319887154bdb3c665626a8df473d449302a
Jan Stola <jan.stola@oracle.com>
2025-05-07 16:33:03 +0200

    DataView.prototype.get/setFloat16 should be enabled in ECMAScript2025.

--- graal-js/src/com.oracle.truffle.js/src/com/oracle/truffle/js/builtins/DataViewPrototypeBuiltins.java
+++ graal-js/src/com.oracle.truffle.js/src/com/oracle/truffle/js/builtins/DataViewPrototypeBuiltins.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, 2024, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2018, 2025, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * The Universal Permissive License (UPL), Version 1.0
@@ -136,7 +136,7 @@ public final class DataViewPrototypeBuiltins extends JSBuiltinsContainer.SwitchE
         @Override
         public int getECMAScriptVersion() {
             return switch (this) {
-                case getFloat16, setFloat16 -> JSConfig.StagingECMAScriptVersion;
+                case getFloat16, setFloat16 -> JSConfig.ECMAScript2025;
                 default -> BuiltinEnum.super.getECMAScriptVersion();
             };
         }
