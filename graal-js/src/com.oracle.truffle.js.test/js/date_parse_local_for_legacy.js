8ee738aacedd57801736d28e988b3c9d0df2f909
Jan Stola <jan.stola@oracle.com>
2025-05-23 10:47:37 +0200

    Testing the interpretation of parsed dates in various modes.
    (cherry picked from commit 3885c1327cfc3837f56b2a4fa43d201b08b18694)
    
    Fix copyright year

---
+++ graal-js/src/com.oracle.truffle.js.test/js/date_parse_local_for_legacy.js
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2025, 2025, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * Licensed under the Universal Permissive License v 1.0 as shown at http://oss.oracle.com/licenses/upl.
+ */
+
+/*
+ * Test of the interpretation of parsed dates (with use-utc-for-legacy-dates=false).
+ *
+ * @option timezone=IST
+ * @option use-utc-for-legacy-dates=false
+ */
+
+load('assert.js');
+load('date_parse_common.js');
+
+assertSame(dateOnlyUTC, zDateOnlyEcma.toString());
+assertSame(dateOnlyUTC, zDateOnlyLegacy.toString());
+
+assertSame(dateTimeUTC, zDateTimeEcma.toString());
+assertSame(dateTimeUTC, zDateTimeLegacy.toString());
+
+assertSame(dateOnlyUTC, dateOnlyEcma.toString());
+assertSame(dateOnlyLocal, dateOnlyLegacy.toString());
+
+assertSame(dateTimeLocal, dateTimeEcma.toString());
+assertSame(dateTimeLocal, dateTimeLegacy.toString());
