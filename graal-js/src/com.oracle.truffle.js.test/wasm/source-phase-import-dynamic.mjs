0a065e98defe0117c5ca18449376914dba5b5474
Andreas Woess <andreas.woess@oracle.com>
2025-06-16 18:34:05 +0200

    Enable source phase imports from WebAssembly modules by default.

--- graal-js/src/com.oracle.truffle.js.test/wasm/source-phase-import-dynamic.mjs
+++ graal-js/src/com.oracle.truffle.js.test/wasm/source-phase-import-dynamic.mjs
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2024, 2025, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * Licensed under the Universal Permissive License v 1.0 as shown at http://oss.oracle.com/licenses/upl.
@@ -8,8 +8,9 @@
 /**
  * Test source phase imports.
  *
- * @option webassembly
- * @option source-phase-imports
+ * @option webassembly=true
+ * @option source-phase-imports=true
+ * @option unhandled-rejections=throw
  */
 
 const wasmModule = await import.source("./source-phase-import.wasm");
