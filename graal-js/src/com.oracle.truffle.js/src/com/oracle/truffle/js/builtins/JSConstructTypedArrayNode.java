eea68d7108c40a54718c0c14e35615dad89556bc
Andreas Woess <andreas.woess@oracle.com>
2025-10-06 16:21:00 +0200

    Replace TypedArray.ElementType with TypedArrayFactory.
    Since there's a 1:1 relationship between the two anyway, there's no need to have both.
    
    (cherry picked from commit 1462e0823a6a42b4a69b295074b9e8c19d90f39c)

--- graal-js/src/com.oracle.truffle.js/src/com/oracle/truffle/js/builtins/JSConstructTypedArrayNode.java
+++ graal-js/src/com.oracle.truffle.js/src/com/oracle/truffle/js/builtins/JSConstructTypedArrayNode.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, 2025, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2018, 2026, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * The Universal Permissive License (UPL), Version 1.0
@@ -292,7 +292,7 @@ public abstract class JSConstructTypedArrayNode extends JSBuiltinNode {
 
         assert typedArray == JSArrayBufferView.typedArrayGetArrayType(result);
 
-        if (bulkCopyProfile.profile(this, !sourceType.isInterop() && sourceType.getElementType() == typedArray.getElementType())) {
+        if (bulkCopyProfile.profile(this, !sourceType.isInterop() && sourceType.getFactory() == typedArray.getFactory())) {
             JSArrayBufferObject srcData = arrayBufferView.getArrayBuffer();
             int sourceByteOffset = arrayBufferView.getByteOffset();
             int elementSize = sourceType.bytesPerElement();
