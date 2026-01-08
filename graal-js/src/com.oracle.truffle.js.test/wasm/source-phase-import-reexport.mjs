4bf1414750b21164b45230d22b93063328f54c6f
Jan Stola <jan.stola@oracle.com>
2026-01-08 14:19:41 +0100

    Testing the import of the re-exported source phase import.

---
+++ graal-js/src/com.oracle.truffle.js.test/wasm/source-phase-import-reexport.mjs
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2026, 2026, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * Licensed under the Universal Permissive License v 1.0 as shown at http://oss.oracle.com/licenses/upl.
+ */
+
+/**
+ * Helper module that re-exports a source phase import.
+ *
+ * @option webassembly=true
+ */
+
+import source mod from "./source-phase-import.wasm";
+
+export { mod };
