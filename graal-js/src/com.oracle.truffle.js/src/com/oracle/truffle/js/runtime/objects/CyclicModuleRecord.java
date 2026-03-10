268bbeb5a53407ad565c21b7ad0249160aeb1811
Andreas Woess <andreas.woess@oracle.com>
2026-03-10 16:21:00 +0100

    Add TruffleBoundary to module record methods used by dynamic imports.

--- graal-js/src/com.oracle.truffle.js/src/com/oracle/truffle/js/runtime/objects/CyclicModuleRecord.java
+++ graal-js/src/com.oracle.truffle.js/src/com/oracle/truffle/js/runtime/objects/CyclicModuleRecord.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024, 2025, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2024, 2026, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * The Universal Permissive License (UPL), Version 1.0
@@ -145,11 +145,13 @@ public abstract class CyclicModuleRecord extends AbstractModuleRecord {
      */
     public abstract Object executeModule(JSRealm realm, PromiseCapabilityRecord capability);
 
+    @TruffleBoundary
     @Override
     public final JSPromiseObject evaluate(JSRealm realm) {
         return context.getEvaluator().moduleEvaluation(realm, this);
     }
 
+    @TruffleBoundary
     @Override
     public final void link(JSRealm realm) {
         context.getEvaluator().moduleLinking(realm, this);
