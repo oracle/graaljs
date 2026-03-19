2f77604e242eeecb54829d32420980e85e761d43
Jan Stola <jan.stola@oracle.com>
2026-03-19 20:31:39 +0100

    Replacing ExplicitResourceManagementOptionTest.java by JavaScript tests.

---
+++ graal-js/src/com.oracle.truffle.js.test/js/explicit-resource-management/enabled.js
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2026, 2026, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * Licensed under the Universal Permissive License v 1.0 as shown at http://oss.oracle.com/licenses/upl.
+ *
+ * @option explicit-resource-management
+ */
+
+load('../assert.js');
+
+(function testEnabled() {
+    let disposed = 0;
+    {
+        using x = { [Symbol.dispose]() { disposed++; } };
+    }
+    assertSame(1, disposed);
+    assertSame('function', typeof DisposableStack);
+    assertSame('symbol', typeof Symbol.dispose);
+})();
+
+(function testUsingScopePreservesNullAndUndefinedErrors() {
+    function throwInBody(value) {
+        try {
+            using x = { [Symbol.dispose]() {} };
+            throw value;
+        } catch (e) {
+            return e;
+        }
+    }
+
+    function throwInDispose(value) {
+        try {
+            using x = { [Symbol.dispose]() { throw value; } };
+        } catch (e) {
+            return e;
+        }
+    }
+
+    assertSame(null, throwInBody(null));
+    assertSame(undefined, throwInBody(undefined));
+    assertSame(null, throwInDispose(null));
+    assertSame(undefined, throwInDispose(undefined));
+})();
+
+(function testForUsingOfRemainsValid() {
+    var using = 0;
+    for (using of [1]) {
+    }
+    assertSame(1, using);
+})();
+
+(function testForUsingOfOfRemainsIdentifierBased() {
+    var using;
+    var of = [[9], [8], [7]];
+    var result = [];
+    for (using of of [0, 1, 2]) {
+        result.push(using);
+    }
+    assertSame(1, result.length);
+    assertSame(7, result[0]);
+})();
