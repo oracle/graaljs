b13050fb86ece7b01775e025bb335a84266ae78c
Jan Stola <jan.stola@oracle.com>
2026-04-07 12:29:42 +0200

    Regression test for comparison against zero after truncation in parseInt.

---
+++ graal-js/src/com.oracle.truffle.js.test/js/GR-74538.js
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 2026, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * Licensed under the Universal Permissive License v 1.0 as shown at http://oss.oracle.com/licenses/upl.
+ */
+
+load("assert.js");
+
+assertSame(-0, parseInt(-0.5, 2));
