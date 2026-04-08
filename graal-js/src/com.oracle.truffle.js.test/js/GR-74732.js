60b696ed2842652a5ea53655c7a9b7860d554550
Jan Stola <jan.stola@oracle.com>
2026-04-08 13:40:53 +0200

    Regression test of Atomics.isLockFree(largeNumber).

---
+++ graal-js/src/com.oracle.truffle.js.test/js/GR-74732.js
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2026, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * Licensed under the Universal Permissive License v 1.0 as shown at http://oss.oracle.com/licenses/upl.
+ */
+
+load("assert.js");
+
+assertFalse(Atomics.isLockFree(4294967297));
+assertFalse(Atomics.isLockFree(-4294967295));
