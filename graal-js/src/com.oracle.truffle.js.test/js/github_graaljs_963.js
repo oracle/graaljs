c2f794439e5e2715602d042189a3c1c9aee41e25
Jan Stola <jan.stola@oracle.com>
2026-05-12 17:50:35 +0200

    Updating github_graaljs_963 test to reflect the changes in PlainYearMonth.

--- graal-js/src/com.oracle.truffle.js.test/js/github_graaljs_963.js
+++ graal-js/src/com.oracle.truffle.js.test/js/github_graaljs_963.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2025, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2025, 2026, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * Licensed under the Universal Permissive License v 1.0 as shown at http://oss.oracle.com/licenses/upl.
@@ -18,4 +18,4 @@ var d1 = Temporal.PlainDate.from('1969-01-01');
 var d2 = Temporal.PlainDate.from('+275760-01-01');
 assertSame(d2.toString(), d1.add(d1.until(d2)).toString());
 
-assertSame('+013606-02', new Temporal.PlainYearMonth(-271821, 5).add(Temporal.Duration.from({milliseconds: 9007199254740991})).toString());
+assertThrows(() => new Temporal.PlainYearMonth(-271821, 5).add(Temporal.Duration.from({milliseconds: 9007199254740991})), RangeError);
