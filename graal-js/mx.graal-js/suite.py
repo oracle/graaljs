7f00b18ae084defb16d8210ffabc1281d51309a5
Marouane El Hallaoui <marouane.el.hallaoui@oracle.com>
2026-07-15 12:41:36 +0100

    new dev cycle GraalVM 25.0.5

--- graal-js/mx.graal-js/suite.py
+++ graal-js/mx.graal-js/suite.py
@@ -4,8 +4,8 @@ suite = {
 
   "name" : "graal-js",
 
-  "version" : "25.0.4",
-  "release" : True,
+  "version" : "25.0.5",
+  "release" : False,
   "groupId" : "org.graalvm.js",
   "url" : "https://www.graalvm.org/javascript",
   "developer" : {
