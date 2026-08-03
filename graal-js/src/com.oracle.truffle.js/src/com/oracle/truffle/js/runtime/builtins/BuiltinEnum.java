1adf506ff55044d6ce988976c3bff380927f330d
Andreas Woess <andreas.woess@oracle.com>
2026-08-03 06:08:22 +0200

    Precompute builtin property key hash code.

--- graal-js/src/com.oracle.truffle.js/src/com/oracle/truffle/js/runtime/builtins/BuiltinEnum.java
+++ graal-js/src/com.oracle.truffle.js/src/com/oracle/truffle/js/runtime/builtins/BuiltinEnum.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, 2024, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2018, 2026, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * The Universal Permissive License (UPL), Version 1.0
@@ -58,7 +58,10 @@ public interface BuiltinEnum<E extends Enum<? extends BuiltinEnum<E>>> {
     }
 
     default Object getKey() {
-        return stripName(Strings.fromJavaString(asEnum().name()));
+        TruffleString key = stripName(Strings.fromJavaString(asEnum().name()));
+        // Precompute hash code so that it is baked into the native image.
+        key.hashCodeUncached(TruffleString.Encoding.UTF_16);
+        return key;
     }
 
     default boolean isConstructor() {
