2ad253675f0ea7d1660de38e8119bd83f6be7795
Andreas Woess <andreas.woess@oracle.com>
2026-08-10 16:58:25 +0200

    Override JavaScriptRootNode.toString().

--- graal-js/src/com.oracle.truffle.js/src/com/oracle/truffle/js/runtime/JavaScriptRootNode.java
+++ graal-js/src/com.oracle.truffle.js/src/com/oracle/truffle/js/runtime/JavaScriptRootNode.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, 2024, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2018, 2026, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * The Universal Permissive License (UPL), Version 1.0
@@ -130,4 +130,9 @@ public abstract class JavaScriptRootNode extends RootNode {
         return JavaScriptLanguage.get(this);
     }
 
+    @Override
+    public String toString() {
+        String className = getClass().getName();
+        return className.substring(className.lastIndexOf('.') + 1);
+    }
 }
